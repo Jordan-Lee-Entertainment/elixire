89694545441bcd784c4928e16f08ade4eb47395f
Luna Mendes <lkmnds@gmail.com>
2018-02-07 00:06:41 -0300

    api.upload: add weekly limit checking
     - api.errors: add Ratelimited

--- api/bp/upload.py
+++ api/bp/upload.py
@@ -5,7 +5,7 @@ from sanic import response
 from ..common_auth import token_check
 from ..common import gen_filename
 from ..snowflake import get_snowflake
-from ..errors import BadImage, BadUpload
+from ..errors import BadImage, BadUpload, Ratelimited
 
 bp = Blueprint('upload')
 
@@ -42,9 +42,31 @@ async def upload_handler(request):
     if filemime not in ACCEPTED_MIMES:
         return BadImage('bad image type')
 
-    filesize = len(filebody)
-    # TODO: check limits using filesize
+    used = await request.app.db.fetchval("""
+    SELECT SUM(file_size)
+    FROM files
+    WHERE uploader = $1
+    AND file_id > time_snowflake(now() - interval '7 hours')
+    """, user_id)
 
+    byte_limit = await request.app.db.fetchval("""
+    SELECT blimit
+    FROM limits
+    WHERE user_id = $1
+    """, user_id)
+
+    if used > byte_limit:
+        cnv_limit = byte_limit / 1024 / 1024
+        raise Ratelimited('You already blew your weekly'
+                          f' limit of {cnv_limit}MB')
+
+    filesize = len(filebody)
+    if used + filesize > byte_limit:
+        cnv_limit = byte_limit / 1024 / 1024
+        raise Ratelimited('This file blows the weekly limit of'
+                          f' {cnv_limit}MB')
+
+    # all good with limits
     file_rname = await gen_filename(request)
     file_id = get_snowflake()
 
