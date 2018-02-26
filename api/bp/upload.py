d842b839b155728f61eb17f0e6644275e434f3ac
Luna Mendes <lkmnds@gmail.com>
2018-02-26 00:03:14 -0300

    api.upload: Add clamdscan virus scanning
     - config.py.example: add ENABLE_FRONTEND, UPLOAD_SCAN and
     UPLOAD_SCAN_WEBHOOK
    
     - requirements: add aiohttp
    
     - run: remove sanic_cors.cross_plugin import
       - add app.session: aiohttp.ClientSession
       - use ENABLE_FRONTEND from config

--- api/bp/upload.py
+++ api/bp/upload.py
@@ -1,4 +1,7 @@
 import pathlib
+import asyncio
+import logging
+import time
 
 from sanic import Blueprint
 from sanic import response
@@ -9,6 +12,7 @@ from ..snowflake import get_snowflake
 from ..errors import BadImage, Ratelimited
 
 bp = Blueprint('upload')
+log = logging.getLogger(__name__)
 
 
 ACCEPTED_MIMES = [
@@ -20,6 +24,85 @@ ACCEPTED_MIMES = [
     'audio/webm',
     'video/webm'
 ]
+
+
+async def scan_webhook(app, user_id: int, filesize: int, scan_out: str):
+    """Execute a discord webhook with information about the virus scan."""
+    uname = await app.db.fetchval("""
+        select username
+        from users
+        where user_id = $1
+    """, user_id)
+
+    webhook_payload = {
+        'embeds': [{
+            'title': 'Elixire Virus Scanning',
+            'color': 0xff0000,
+            'fields': [
+                {
+                    'name': 'user',
+                    'value': f'id: {user_id}, username: {uname}'
+                },
+
+                {
+                    'name': 'file size',
+                    'value': f'{filesize} bytes'
+                },
+
+                {
+                    'name': 'clamdscan out',
+                    'value': f'```\n{scan_out}\n```'
+                }
+                
+            ]
+        }],
+    }
+
+    async with app.session.post(app.econfig.UPLOAD_SCAN_WEBHOOK,
+                                json=webhook_payload) as resp:
+        return resp
+
+
+async def scan_file(request, **kwargs):
+    """Scan a file for viruses using clamdscan.
+
+    Raises BadImage on any non-successful scan.
+    """
+    filebody = kwargs.get('filebody')
+    filesize = kwargs.get('filesize')
+    user_id = kwargs.get('user_id')
+
+    app = request.app
+
+    if not request.app.econfig.UPLOAD_SCAN:
+        log.warning('Scans are disabled.')
+        return
+
+    # time to check clamdscan
+    scanstart = time.monotonic()
+    scanproc = await asyncio.create_subprocess_shell(
+        "clamdscan -i -m --no-summary -",
+        stderr=asyncio.subprocess.PIPE,
+        stdout=asyncio.subprocess.PIPE,
+        stdin=asyncio.subprocess.PIPE,
+    )
+
+    # combine outputs
+    out, err = map(lambda s: s.decode('utf-8'),
+                   await scanproc.communicate(input=filebody))
+    scanend = time.monotonic()
+
+    delta = round(scanend - scanstart, 6)
+    log.info(f'Scanning {filesize/1024/1024} MB took {delta} seconds')
+
+    complete = f'{out}{err}'
+    log.debug(f'output of clamdscan: {complete}')
+
+    if 'OK' not in complete:
+        # Oops.
+        log.warning(f'user id {user_id} did a dumb')
+        await scan_webhook(app, user_id, filesize, complete)
+        raise BadImage('Image contains a virus.')
 
 
 @bp.post('/api/upload')
@@ -73,6 +156,9 @@ async def upload_handler(request):
                           f' {cnv_limit}MB')
 
     # all good with limits
+    await scan_file(request,
+                    filebody=filebody, filesize=filesize, user_id=user_id)
+
     file_rname = await gen_filename(request)
     file_id = get_snowflake()
 
