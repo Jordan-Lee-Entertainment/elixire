c1ab98517566d7da3eb5738547d57643d14408ad
Ave O <ave@ave.zone>
2018-02-27 03:40:23 +0300

    Try and fix bug

--- api/bp/files.py
+++ api/bp/files.py
@@ -55,7 +55,7 @@ async def delete_handler(request):
     SELECT uploader, fspath
     FROM files
     WHERE filename = $1
-    AND deleted = false
+    AND deleted != true
     """, file_name)
 
     if not file_info or file_info["uploader"] != user_id:
