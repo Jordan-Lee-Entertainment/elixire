c50fa47ee57c86e0e8276b592401cb2250ee231f
Ave O <ave@ave.zone>
2018-02-28 20:12:00 +0300

    Add shorten functionality
    - Add /s/<shortenedlink>
    - Add /api/shorten
    - Add proper limits to DB
    - Add a new table for shortened files
    - Add limit output to profile
    - Fix bug with limit checks on upload/shorten

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ import api.bp.auth
 import api.bp.profile
 import api.bp.upload
 import api.bp.files
+import api.bp.shorten
 
 from api.errors import APIError
 
@@ -20,14 +21,15 @@ import config
 app = Sanic()
 app.econfig = config
 
-# enable cors on api and images
-CORS(app, resources=[r"/api/*", r"/i/*"])
+# enable cors on api, images and shortens
+CORS(app, resources=[r"/api/*", r"/i/*", r"/s/*"])
 
 # load blueprints
 app.blueprint(api.bp.auth.bp)
 app.blueprint(api.bp.profile.bp)
 app.blueprint(api.bp.upload.bp)
 app.blueprint(api.bp.files.bp)
+app.blueprint(api.bp.shorten.bp)
 # TODO: app.blueprint(api.bp.fetch.bp)
 
 logging.basicConfig(level=logging.DEBUG)
