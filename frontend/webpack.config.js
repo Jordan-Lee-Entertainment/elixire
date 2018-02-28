f32d90f441fd172dd068ec5fb4ef5138764d4c7c
heatingdevice <ipadlover8322@gmail.com>
2018-02-27 21:21:11 -0500

    Move account info to right, add file list, fix spacing on about page

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -37,6 +37,10 @@ const pageList = [
   {
     title: "About Us | Elixire",
     chunkName: "about"
+  },
+  {
+    title: "My Files | Elixire",
+    chunkName: "list"
   }
 ];
 
@@ -55,7 +59,8 @@ module.exports = {
     theme: `${SRC_DIR}/theme.js`,
     bootstrapJs: "bootstrap",
     themeCSS: `${SRC_DIR}/themeCSS.js`,
-    about: `${SRC_DIR}/about.js`
+    about: `${SRC_DIR}/about.js`,
+    list: `${SRC_DIR}/list.js`
   },
   output: {
     filename: "assets/[chunkhash].js",
