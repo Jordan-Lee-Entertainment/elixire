17e423eeb46375f77809a9cf889ce73b9a87812a
Ave O <ave@ave.zone>
2018-02-28 21:42:18 +0300

    Switch to fetch
    - Switch from a static link to fetch
    - Rewrite /api/delete to remove unnecessary file move (also makes one less DB call than before if CF purge is disabled)

--- run.py
+++ run.py
@@ -13,6 +13,7 @@ import api.bp.profile
 import api.bp.upload
 import api.bp.files
 import api.bp.shorten
+import api.bp.fetch
 
 from api.errors import APIError
 
@@ -30,7 +31,7 @@ app.blueprint(api.bp.profile.bp)
 app.blueprint(api.bp.upload.bp)
 app.blueprint(api.bp.files.bp)
 app.blueprint(api.bp.shorten.bp)
-# TODO: app.blueprint(api.bp.fetch.bp)
+app.blueprint(api.bp.fetch.bp)
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
@@ -86,7 +87,7 @@ def main():
             pass
 
     # TODO: b2 / s3 support ????
-    app.static('/i', './images')
+    # app.static('/i', './images')
 
     if config.ENABLE_FRONTEND:
         app.static('/', './frontend/output')
