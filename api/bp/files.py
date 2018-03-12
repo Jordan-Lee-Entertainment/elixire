a773b1cd67a5fc586c11fe430c9256904058631a
Ave O <ave@ave.zone>
2018-03-12 04:28:20 +0300

    Get delete/shortendelete working with multidomain

--- api/bp/files.py
+++ api/bp/files.py
@@ -56,21 +56,15 @@ async def list_handler(request):
     })
 
 
-async def purge_cf_cache(app, file_name: str, base_urls):
-    """Clear the Cloudflare cache for the given URL."""
-
-    if not app.econfig.CF_PURGE:
-        log.warning('Cloudflare purging is disabled.')
-        return
+async def _purge_cf_cache(app, purge_urls, email, apikey, zoneid):
+    """Clear the Cloudflare cache for the given URLs and cf creds."""
 
     cf_purge_url = "https://api.cloudflare.com/client/v4/zones/"\
-                   f"{app.econfig.CF_ZONEID}/purge_cache"
-
-    purge_urls = [file_url+file_name for file_url in base_urls]
+                   f"{zoneid}/purge_cache"
 
     cf_auth_headers = {
-        'X-Auth-Email': app.econfig.CF_EMAIL,
-        'X-Auth-Key': app.econfig.CF_APIKEY
+        'X-Auth-Email': email,
+        'X-Auth-Key': apikey
     }
 
     purge_payload = {
@@ -81,6 +75,54 @@ async def purge_cf_cache(app, file_name: str, base_urls):
                                   json=purge_payload,
                                   headers=cf_auth_headers) as resp:
         return resp
+
+
+# TODO: I tried, I really tried, but i can't seem to reduce the code
+# repetition with the following two functions without adding more DB calls
+# and I don't want even more DB calls
+async def purge_cf_cache_file(app, filename: str):
+    file_detail = await app.db.fetchrow("""
+    SELECT domain, fspath
+    FROM files
+    WHERE filename = $1
+    """, filename)
+
+    domain_detail = await app.db.fetchrow("""
+    SELECT domain, cf_enabled, cf_email, cf_zoneid, cf_apikey
+    FROM domains
+    WHERE domain_id = $1
+    """, file_detail["domain"])
+
+    # Checking if purge is enabled
+    if domain_detail["cf_enabled"]:
+        purge_url = f"https://{domain_detail['domain']}/i/"\
+                    f"{os.path.basename(file_detail['fspath'])}"
+
+        await _purge_cf_cache(app, [purge_url], domain_detail["cf_email"],
+                              domain_detail["cf_apikey"],
+                              domain_detail["cf_zoneid"])
+
+
+async def purge_cf_cache_shorten(app, filename: str):
+    shorten_detail = await app.db.fetchval("""
+    SELECT domain
+    FROM shortens
+    WHERE filename = $1
+    """, filename)
+
+    domain_detail = await app.db.fetchrow("""
+    SELECT domain, cf_enabled, cf_email, cf_zoneid, cf_apikey
+    FROM domains
+    WHERE domain_id = $1
+    """, shorten_detail["domain"])
+
+    # Checking if purge is enabled
+    if domain_detail["cf_enabled"]:
+        purge_url = f"https://{domain_detail['domain']}/s/{filename}"
+
+        await _purge_cf_cache(app, [purge_url], domain_detail["cf_email"],
+                              domain_detail["cf_apikey"],
+                              domain_detail["cf_zoneid"])
 
 
 @bp.delete('/api/delete')
@@ -101,20 +143,7 @@ async def delete_handler(request):
     if exec_out == "UPDATE 0":
         raise NotFound('You have no files with this name.')
 
-    # Checking if purge is enabled here too
-    # So we can prevent an extra API call if it is not necessary
-    if request.app.econfig.CF_PURGE:
-        file_path = await request.app.db.fetchval("""
-        SELECT fspath
-        FROM files
-        WHERE filename = $1
-        AND deleted = true
-        """, file_name)
-
-        full_filename = os.path.basename(file_path)
-
-        await purge_cf_cache(request.app, full_filename,
-                             request.app.econfig.CF_UPLOADURLS)
+    await purge_cf_cache_file(request.app, file_name)
 
     return response.json({
         'success': True
@@ -140,8 +169,7 @@ async def shortendelete_handler(request):
     if exec_out == "UPDATE 0":
         raise NotFound('You have no shortens with this name.')
 
-    await purge_cf_cache(request.app, file_name,
-                         request.app.econfig.CF_SHORTENURLS)
+    await purge_cf_cache_shorten(request.app, file_name)
 
     return response.json({
         'success': True
