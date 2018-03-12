6b6a9caa8291a54233a531a2337d96aeac29951b
Ave O <ave@ave.zone>
2018-03-12 05:42:23 +0300

    add renamefile util

---
+++ utils/renamefile.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3.6
+import sys
+import asyncio
+
+import asyncpg
+
+sys.path.append('..')
+import config
+
+
+async def main():
+    db = await asyncpg.create_pool(**config.db)
+    old_filename = sys.argv[1]
+    new_filename = sys.argv[2]
+
+    exec_out = await db.execute("""
+    UPDATE files
+    SET filename = $1
+    WHERE filename = $2
+    AND deleted = false
+    """, new_filename, old_filename)
+
+    print(f"db out: {exec_out}")
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
