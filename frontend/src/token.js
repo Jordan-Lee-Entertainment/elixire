35ed37eb58c376eb2d26bb352db1130cd21c156b
Mary <ipadlover8322@gmail.com>
2018-03-13 11:55:02 +0000

    webpack v4 support

--- frontend/src/token.js
+++ frontend/src/token.js
@@ -1,17 +1,15 @@
 import "./token.scss";
 import uploaders from "./uploaders.js";
+import "highlightjs/styles/solarized-light.css";
 
 window.addEventListener("load", async function() {
   const token = window.location.hash.substring(1);
   document.getElementById("token").innerText = token;
   const kshareConfig = uploaders.kshareConfig(token);
   const sharexConfig = uploaders.sharexConfig(token);
-  const elixireManager = uploaders.elixireManager(token);
 
   document.getElementById("kshare-config").innerText = kshareConfig;
   document.getElementById("sharex-config").innerText = sharexConfig;
-  highlight.highlightBlock(document.getElementById("sharex-config"));
-  highlight.highlightBlock(document.getElementById("kshare-config"));
 
   const kshareDl = document.getElementById("kshare-dl");
   kshareDl.href = URL.createObjectURL(
@@ -23,9 +21,18 @@ window.addEventListener("load", async function() {
     new Blob([sharexConfig], { type: "application/json" })
   );
   sharexDl.download = "elixire.sxcu";
-  const elixireManagerDl = document.getElementById("elixire-manager-dl");
-  elixireManagerDl.href = URL.createObjectURL(
-    new Blob([elixireManager], { type: "application/json" })
-  );
-  elixireManagerDl.download = "elixiremanager.sh";
+  // This way we can still await the highlight deps
+  window.profilePromise.then(() => {
+    const elixireManager = uploaders.elixireManager(token);
+    const elixireManagerDl = document.getElementById("elixire-manager-dl");
+    elixireManagerDl.href = URL.createObjectURL(
+      new Blob([elixireManager], { type: "application/json" })
+    );
+    elixireManagerDl.download = "elixiremanager.sh";
+  });
+
+  const highlight = await import("highlightjs/highlight.pack.js");
+  highlight.highlightBlock(document.getElementById("sharex-config"));
+  highlight.highlightBlock(document.getElementById("kshare-config"));
+  console.log("highlighted!");
 });
