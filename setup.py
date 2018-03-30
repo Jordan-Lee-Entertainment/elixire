fa71a527c9258d40176a73c0a15a8ed299826526
Luna Mendes <lkmnds@gmail.com>
2018-03-30 17:20:20 -0300

    all: basics of backend testing

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='elixire',
+    version='2',
+    description='Image host',
+    url='https://elixi.re',
+    author='Ave Ozkal, Luna Mendes, Mary Strodl',
+    python_requires='>=3.6'
+)
---
+++ tests/test_test.py
@@ -0,0 +1,5 @@
+def test_stuff():
+    # The world should be fine
+    # If this test fail... I don't think this is a coherent universe
+    # send help
+    assert 1 == 1
