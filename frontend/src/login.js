454a3ba7399590522beb33c46c3f2c5dc1e6122c
heatingdevice <ipadlover8322@gmail.com>
2018-04-06 07:41:33 -0400

    Add remember me feature

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -5,6 +5,25 @@ import "./forms.scss";
 window.addEventListener("load", async function() {
   const username = document.getElementById("username");
   const password = document.getElementById("password");
+  const keepLogin = document.getElementById("keep-logged-in");
+  const keepLoginBtn = document.getElementById("save-password");
+  keepLogin.addEventListener("change", function() {
+    if (!keepLogin.checked) {
+      return;
+    }
+    keepLogin.checked = false;
+    const btn = document.createElement("button");
+    btn.setAttribute("data-toggle", "modal");
+    btn.setAttribute("data-target", "#remember-modal");
+    btn.classList = "btn btn-primary";
+    btn.type = "button";
+    btn.style.display = "none";
+    document.body.appendChild(btn);
+    btn.click();
+  });
+  keepLoginBtn.addEventListener("click", function() {
+    keepLogin.checked = true;
+  });
   const submit = document.getElementById("submit-btn");
   let alertId = null;
   username.focus();
@@ -18,10 +37,9 @@ window.addEventListener("load", async function() {
   async function login() {
     if (alertId) commonCode.removeAlert(alertId);
     try {
-      const token = await commonCode.client.login(
-        username.value,
-        password.value
-      );
+      const token = keepLogin.checked
+        ? await commonCode.client.generateToken(password.value, username.value)
+        : await commonCode.client.login(username.value, password.value);
       window.localStorage.setItem("token", token);
       window.location.pathname = window.location.hash.substring(1) || "/";
     } catch (err) {
