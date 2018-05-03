e781f1c2f3e7b8d25f18c1b50fd813ccf6681c8a
Luna Mendes <lkmnds@gmail.com>
2018-05-02 21:23:57 -0300

    api: add register blueprint
    api.admin:
     - add paging to list_users_handler
     - add activate_user
     - add deactivate_user
    
    api.errors:
     - add get_payload to BadInput
    
    api.schema:
     - add custom ElixireValidator
     - add REGISTRATION_SCHEMA
    
    example config:
     - add USER_REGISTER_WEBHOOK

--- api/bp/admin.py
+++ api/bp/admin.py
@@ -5,7 +5,7 @@ elixire - admin routes
 from sanic import Blueprint, response
 
 from ..common_auth import token_check, check_admin
-from ..errors import NotFound
+from ..errors import NotFound, BadInput
 
 
 bp = Blueprint('admin')
@@ -30,12 +30,18 @@ async def list_users_handler(request):
     user_id = await token_check(request)
     await check_admin(request, user_id, True)
 
-    # TODO: this is inefficient.
-    # how to approach paging, in something like SQL?
+    try:
+        page = int(request.json['page'])
+    except (TypeError, ValueError):
+        raise BadInput('Invalid page integer.')
+
+    # hmmm maybe more users per page?
     data = await request.app.db.fetch("""
     SELECT user_id, username, active, admin, domain
     FROM users
-    """)
+    LIMIT 15
+    OFFSET ($1 * 15)
+    """, page)
 
     return response.json(list(map(dict, data)))
 
@@ -55,3 +61,41 @@ async def get_user_handler(request, user_id):
         raise NotFound('User not found')
 
     return response.json(dict(udata))
+
+@bp.post('/api/admin/activate/<user_id>')
+async def activate_user(request, user_id: int):
+    """Activate one user, given its ID."""
+    user_id = await token_check(request)
+    await check_admin(request, user_id, True)
+
+    result = await request.app.db.execute("""
+    UPDATE users
+    SET active = false
+    WHERE user_id = $1
+    """, user_id)
+
+    if result == "UPDATE 0":
+        raise BadInput('Provided user ID does not reference any user.')
+
+    return response.json({
+        'success': True
+    })
+
+@bp.post('/api/admin/deactivate/<user_id>')
+async def deactivate_user(request, user_id: int):
+    """Deactivate one user, given its ID."""
+    user_id = await token_check(request)
+    await check_admin(request, user_id, True)
+
+    result = await request.app.db.execute("""
+    UPDATE users
+    SET active = false
+    WHERE user_id = $1
+    """, user_id)
+
+    if result == "UPDATE 0":
+        raise BadInput('Provided user ID does not reference any user.')
+
+    return response.json({
+        'success': True
+    })
--- api/schema.py
+++ api/schema.py
@@ -1,14 +1,31 @@
 """Common schema information."""
+import re
 
 from cerberus import Validator
 from .errors import BadInput
 
+USERNAME_REGEX = re.compile(r'^[a-z]{1}[a-zA-Z0-9_]{2,19}$', re.M | re.A)
+
+class ElixireValidator(Validator):
+    def _validate_type_username(self, value) -> bool:
+        """Validate usernames."""
+        # re.match returns none, soooo, bool(None) = False
+        return bool(USERNAME_REGEX.match(value))
+
+    def _validate_type_password(self, value) -> bool:
+        """Validate passwords.
+
+         - More than 8 characters.
+         - Less than 100.
+        """
+        # Would it be interesting to measure entropy?
+        return len(value) > 8 and len(value) < 100
 
 def validate(document, schema):
     """Validate one document against a schema."""
-    v = Validator(schema)
-    if not v.validate(document):
-        raise BadInput('Bad payload', v.errors)
+    validator = ElixireValidator(schema)
+    if not validator.validate(document):
+        raise BadInput('Bad payload', validator.errors)
 
     return document
 
@@ -18,4 +35,10 @@ PROFILE_SCHEMA = {
     'password': {'type': 'string'},
     'new_password': {'type': 'string', 'nullable': True},
     'domain': {'type': 'integer', 'nullable': True},
+}
+
+REGISTRATION_SCHEMA = {
+    'username': {'type': 'username'},
+    'password': {'type': 'password'},
+    'discord_user': {'type': 'string'},
 }
