d2b92807b29f4e5a8e4b6d91aba569c3ae794bf8
Luna Mendes <lkmnds@gmail.com>
2018-05-10 00:20:22 -0300

    tests: add domain tests

---
+++ tests/test_domains.py
@@ -0,0 +1,47 @@
+import pytest
+import sys
+import os
+sys.path.append(os.getcwd())
+
+from elixire.run import app as mainapp
+import elixire.tests.creds
+from elixire.tests.common import token, username, login_normal, login_admin
+
+
+@pytest.yield_fixture
+def app():
+    yield mainapp
+
+
+@pytest.fixture
+def test_cli(loop, app, test_client):
+    return loop.run_until_complete(test_client(app))
+
+async def assert_domains(resp):
+    assert resp.status == 200
+
+    rjson = await resp.json()
+    assert isinstance(rjson, dict)
+    assert isinstance(rjson['domains'], dict)
+
+
+async def test_domains_nouser(test_cli):
+    resp = await test_cli.get('/api/domains')
+    await assert_domains(resp)
+
+
+async def test_domains_user(test_cli):
+    utoken = await login_normal(test_cli)
+    resp = await test_cli.get('/api/domains', headers={
+        'Authorization': utoken,
+    })
+
+    await assert_domains(resp)
+
+
+async def test_domains_admin(test_cli):
+    atoken = await login_admin(test_cli)
+    resp = await test_cli.get('/api/domains', headers={
+        'Authorization': atoken,
+    })
+    await assert_domains(resp)
