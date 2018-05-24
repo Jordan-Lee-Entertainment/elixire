c1aa4032e7655d07b4ff2862acc5cbe7c9252e7e
Luna Mendes <lkmnds@gmail.com>
2018-05-24 16:09:04 -0300

    api.datadump: add some basics
    Former-commit-id: f43554473380b09d4420d06a2fbb711ddc6af01b

--- api/bp/datadump.py
+++ api/bp/datadump.py
@@ -3,7 +3,7 @@ import logging
 from sanic import Blueprint, response
 
 from ..common_auth import token_check, check_admin
-from ..errors import NotFound, BadInput
+from ..errors import BadInput
 
 log = logging.getLogger(__name__)
 bp = Blueprint('datadump')
@@ -29,10 +29,45 @@ async def request_data_dump(request):
 async def data_dump_user_status(request):
     """Give information about the current dump for the user,
     if one exists."""
-    pass
+    user_id = await token_check(request)
+
+    row = await request.app.db.fetchrow("""
+    SELECT user_id, start_timestamp, current_id, last_id, total_files, files_done
+    FROM current_dump_state
+    WHERE user_id = $1
+    """, user_id)
+
+    if not row:
+        # TODO: show current state in queue, if any
+        raise BadInput('Your dump is not being processed right now.')
+
+    return response.json({
+        'start_timestamp': row['start_timestamp'].isotimestamp(),
+        'current_id': row['current_id'],
+        'last_id': row['last_id'],
+        'total_files': row['total_files'],
+        'files_done': row['files_done']
+    })
 
 
 @bp.get('/api/dump/global_status')
 async def data_dump_global_status(request):
     """Only for admins: all stuff related to data dump state."""
-    pass
+    user_id = await token_check(request)
+    await check_admin(request, user_id, True)
+
+    queue = await request.app.db.fetch("""
+    SELECT user_id FROM dump_queue
+    """)
+
+    queue = [el['user_id'] for el in queue]
+
+    current = await request.app.db.fetchrow("""
+    SELECT user_id, total_files, files_done
+    FROM current_dump_state
+    """)
+
+    return response.json({
+        'queue': queue,
+        'current': dict(current or {})
+    })
