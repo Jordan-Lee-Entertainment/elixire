f808bc70c4fe771d8ddeecee17db5c47c597f1e5
Luna Mendes <lkmnds@gmail.com>
2018-05-26 17:19:18 -0300

    utils/stats: use consented field
    We still need the non-consented users' statistics privately, to provide
    a better service quality.
    
    Closes #28
    
    
    Former-commit-id: cc72c1a76b06b78cfb6d33bec950be3f1c13bab6

--- utils/stats.py
+++ utils/stats.py
@@ -19,6 +19,10 @@ def byte_to_mibstring(bytecount):
 async def main():
     db, _redis = await open_db()
 
+    files_user_join = 'JOIN users ON files.uploader = users.user_id'
+    shorten_user_join = 'JOIN users ON shortens.uploader = users.user_id'
+    user_c_criteria = 'AND users.consented = true'
+
     # Total domain with cf enabled
     cf_domain_count = await db.fetchval("""
     SELECT COUNT(*)
@@ -40,11 +44,25 @@ async def main():
     WHERE deleted = false
     """)
 
+    nd_file_count_c = await db.fetchval(f"""
+    SELECT COUNT(*)
+    FROM files
+    {files_user_join}
+    WHERE deleted = false {user_c_criteria}
+    """)
+
     # Total deleted file count
     d_file_count = await db.fetchval("""
     SELECT COUNT(*)
     FROM files
     WHERE deleted = true
+    """)
+
+    d_file_count_c = await db.fetchval(f"""
+    SELECT COUNT(*)
+    FROM files
+    {files_user_join}
+    WHERE deleted = true {user_c_criteria}
     """)
 
     # Total non-deleted shortens
@@ -54,11 +72,25 @@ async def main():
     WHERE deleted = false
     """)
 
+    nd_shorten_count_c = await db.fetchval(f"""
+    SELECT COUNT(*)
+    FROM shortens
+    {shorten_user_join}
+    WHERE deleted = false {user_c_criteria}
+    """)
+
     # Total deleted shortens
     d_shorten_count = await db.fetchval("""
     SELECT COUNT(*)
     FROM shortens
     WHERE deleted = true
+    """)
+
+    d_shorten_count_c = await db.fetchval(f"""
+    SELECT COUNT(*)
+    FROM shortens
+    {shorten_user_join}
+    WHERE deleted = true {user_c_criteria}
     """)
 
     # Total non-deleted file size
@@ -68,11 +100,25 @@ async def main():
     WHERE deleted = false
     """)
 
+    total_nd_file_size_c = await db.fetchval(f"""
+    SELECT SUM(file_size)
+    FROM files
+    {files_user_join}
+    WHERE deleted = false {user_c_criteria}
+    """)
+
     # Total deleted file size
     total_d_file_size = await db.fetchval("""
     SELECT SUM(file_size)
     FROM files
     WHERE deleted = true
+    """)
+
+    total_d_file_size_c = await db.fetchval(f"""
+    SELECT SUM(file_size)
+    FROM files
+    {files_user_join}
+    WHERE deleted = true {user_c_criteria}
     """)
 
     # Total non-deleted file uploads in last week
@@ -83,12 +129,28 @@ async def main():
     AND deleted = false
     """)
 
+    nd_file_count_week_c = await db.fetchval(f"""
+    SELECT COUNT(*)
+    FROM files
+    {files_user_join}
+    WHERE file_id > time_snowflake(now() - interval '7 days')
+    AND deleted = false {user_c_criteria}
+    """)
+
     # Total deleted file uploads in last week
     d_file_count_week = await db.fetchval("""
     SELECT COUNT(*)
     FROM files
     WHERE file_id > time_snowflake(now() - interval '7 days')
     AND deleted = true
+    """)
+
+    d_file_count_week_c = await db.fetchval(f"""
+    SELECT COUNT(*)
+    FROM files
+    {files_user_join}
+    WHERE file_id > time_snowflake(now() - interval '7 days')
+    AND deleted = true {user_c_criteria}
     """)
 
     # Total size of non-deleted file uploads in last week
@@ -99,12 +161,28 @@ async def main():
     AND deleted = false
     """)
 
+    total_nd_file_size_week_c = await db.fetchval(f"""
+    SELECT SUM(file_size)
+    FROM files
+    {files_user_join}
+    WHERE file_id > time_snowflake(now() - interval '7 days')
+    AND deleted = false {user_c_criteria}
+    """)
+
     # Total size of deleted file uploads in last week
     total_d_file_size_week = await db.fetchval("""
     SELECT SUM(file_size)
     FROM files
     WHERE file_id > time_snowflake(now() - interval '7 days')
     AND deleted = true
+    """)
+
+    total_d_file_size_week_c = await db.fetchval(f"""
+    SELECT SUM(file_size)
+    FROM files
+    {files_user_join}
+    WHERE file_id > time_snowflake(now() - interval '7 days')
+    AND deleted = true {user_c_criteria}
     """)
 
     # Total non-deleted shortens in last week
@@ -115,6 +193,14 @@ async def main():
     AND deleted = false
     """)
 
+    nd_shorten_count_week_c = await db.fetchval(f"""
+    SELECT COUNT(*)
+    FROM shortens
+    {shorten_user_join}
+    WHERE shorten_id > time_snowflake(now() - interval '7 days')
+    AND deleted = false {user_c_criteria}
+    """)
+
     # Total deleted shortens in last week
     d_shorten_count_week = await db.fetchval("""
     SELECT COUNT(*)
@@ -122,6 +208,15 @@ async def main():
     WHERE shorten_id > time_snowflake(now() - interval '7 days')
     AND deleted = true
     """)
+
+    d_shorten_count_week_c = await db.fetchval(f"""
+    SELECT COUNT(*)
+    FROM shortens
+    {shorten_user_join}
+    WHERE shorten_id > time_snowflake(now() - interval '7 days')
+    AND deleted = true {user_c_criteria}
+    """)
+
 
     # Total active user count
     total_active_user_count = await db.fetchval("""
@@ -175,11 +270,21 @@ Total non-CF domains: {ncf_domain_count}
 Files
 =====
 Global Counts, ND: {nd_file_count}, D: {d_file_count}
+Global Counts (Public), ND: {nd_file_count_c}, D: {d_file_count_c}
+
 Weekly Counts, ND: {nd_file_count_week}, D: {d_file_count_week}
+Weekly Counts (Public), ND: {nd_file_count_week_c}, D: {d_file_count_week_c}
+
 Global sizes, ND: {byte_to_mibstring(total_nd_file_size)}, \
 D: {byte_to_mibstring(total_d_file_size)}
+Global sizes (Public), ND: {byte_to_mibstring(total_nd_file_size_c)}, \
+D: {byte_to_mibstring(total_d_file_size_c)}
+
 Weekly sizes, ND: {byte_to_mibstring(total_nd_file_size_week)}, \
 D: {byte_to_mibstring(total_d_file_size_week)}
+Weekly sizes (Public), ND: {byte_to_mibstring(total_nd_file_size_week_c)}, \
+D: {byte_to_mibstring(total_d_file_size_week_c)}
+
 Biggest file: '{biggest_file['filename']}{biggest_ext}' \
 at {byte_to_mibstring(biggest_file['file_size'])}
 Smallest file: '{smallest_file['filename']}{smallest_ext}' \
@@ -188,7 +293,10 @@ at {byte_to_mibstring(smallest_file['file_size'])}
 Shortens
 ========
 Global Counts, ND: {nd_shorten_count}, D: {d_shorten_count}
+Global Counts (Public), ND: {nd_shorten_count_c}, D: {d_shorten_count_c}
+
 Weekly Counts, ND: {nd_shorten_count_week}, D: {d_shorten_count_week}
+Weekly Counts (Public), ND: {nd_shorten_count_week_c}, D: {d_shorten_count_week_c}
     """)
 
     await close_db(db, _redis)
