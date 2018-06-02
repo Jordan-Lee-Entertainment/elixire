dc5e9abf72930faea639d446500d7fe0053a804f
Luna Mendes <lkmnds@gmail.com>
2018-06-02 17:30:43 -0300

    schema.sql: fix typo
    Former-commit-id: 87db3c621835b6606355a7affd54a6663477d147

--- schema.sql
+++ schema.sql
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS users (
     domain bigint REFERENCES domains (domain_id) DEFAULT 0,
 
     shorten_subdomain text DEFAULT '',
-    shroten_domain bigint REFERENCES domains (domain_id) DEFAULT NULL
+    shorten_domain bigint REFERENCES domains (domain_id) DEFAULT NULL
 );
 
 -- user and IP bans, usually automatically managed by
