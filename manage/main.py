c89c127f557838faee54faf9e0785a3ddf6cd2e0
Luna Mendes <lkmnds@gmail.com>
2018-06-28 17:00:08 -0300

    manage: add help and adduser operations
    Former-commit-id: edc4799862e07f6bfbf76d59384e631220ef776c

--- manage/main.py
+++ manage/main.py
@@ -1,15 +1,20 @@
 import argparse
+import secrets
 import asyncio
 import sys
 from collections import namedtuple
 
+import bcrypt
 import asyncpg
 import aioredis
+
+from api.snowflake import get_snowflake
 
 Context = namedtuple('ArgContext', 'args db redis')
 
 
 async def connect_db(config, loop):
+    """Connect to databases"""
     pool = await asyncpg.create_pool(**config.db)
     redis = await aioredis.create_redis_pool(
         config.redis,
@@ -20,21 +25,94 @@ async def connect_db(config, loop):
     return pool, redis
 
 
-async def list_ops(args):
+async def close_ctx(ctx):
+    """Close database connections."""
+    await ctx.db.close()
+    ctx.redis.close()
+    await ctx.redis.wait_closed()
+
+
+async def list_ops(_ctx):
+    """list
+
+    List all available operations on the manage script.
+    """
     print('All available operations:')
     for name in OPERATIONS:
         desc, _ = OPERATIONS[name]
         print(f'\t{name}: {desc}')
 
 
+async def manage_help(ctx):
+    """help <operation>
+
+    Get help for a certain operation.
+    """
+    try:
+        operation = ctx.args.args[0]
+    except IndexError:
+        await list_ops(ctx)
+        return
+
+    try:
+        _desc, func = OPERATIONS[operation]
+    except KeyError:
+        return print('unknown operation')
+
+    print(func.__doc__)
+
+
+async def adduser(ctx):
+    """adduser <email> <username> [password]
+
+    Add a single user on the current instance.
+    When password is not provided, a secure one is generated.
+    """
+    try:
+        email, username, = ctx.args.args[:2]
+    except ValueError:
+        return print('invalid number of arguments')
+
+    try:
+        password = ctx.args.args[2]
+    except IndexError:
+        password = secrets.token_urlsafe(25)
+
+    pass_hashing = password.encode()
+    hashed = bcrypt.hashpw(pass_hashing, bcrypt.gensalt(14))
+
+    user_id = get_snowflake()
+
+    await ctx.db.execute("""
+    INSERT INTO users (user_id, username, password_hash, email)
+    VALUES ($1, $2, $3, $4)
+    """, user_id, username, hashed.decode(), email)
+
+    await ctx.db.execute("""
+    INSERT INTO limits (user_id)
+    VALUES ($1)
+    """, user_id)
+
+    await ctx.redis.delete(f'uid:{username}')
+
+    print(f"""
+    user id: {user_id}
+    username: {username!r}
+    password: {password!r}
+    """)
+
+
 OPERATIONS = {
     'list': ('List all available operations', list_ops),
+    'help': ('Get help for an operation', manage_help),
+    'adduser': ('Add a user into the instance', adduser),
 }
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('operation', help='a management operation '
                                       '(list lists all available operations)')
+parser.add_argument('args', help='arguments to the operation', nargs='*')
 
 
 def main(config):
@@ -48,10 +126,11 @@ def main(config):
     db, redis = loop.run_until_complete(connect_db(config, loop))
 
     ctx = Context(args, db, redis)
-    print(ctx)
 
     try:
         _desc, func = OPERATIONS[args.operation]
         loop.run_until_complete(func(ctx))
     except KeyError:
         print('invalid operation')
+
+    loop.run_until_complete(close_ctx(ctx))
