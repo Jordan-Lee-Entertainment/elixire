41f54721e82e37728164dad1a9b6b0c8cc171a33
Luna Mendes <lkmnds@gmail.com>
2018-06-28 03:42:36 -0300

    Add humans.txt
    With cool text. Because I can.
    
    We should get a robots.txt at some point.
    
    
    Former-commit-id: 4219e609d461aa764992693fa08945fa2774b4ad

--- run.py
+++ run.py
@@ -404,6 +404,8 @@ def main():
     # app.static('/i', './images')
 
     if config.ENABLE_FRONTEND:
+        app.static('/humans.txt', './humans.txt')
+
         app.static('/admin', './admin-panel/dist')
         app.static('/admin', './admin-panel/dist/index.html')
 
