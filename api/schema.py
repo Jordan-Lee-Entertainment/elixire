ba994c85b4a3016cc41dc4d37f1ad458afb94059
Ave O <ave@ave.zone>
2018-06-30 19:33:46 +0300

    Improve subdomain regex
    subdomain regex: now 1000x more chaotic
    
    
    Former-commit-id: 827c59fe8b8fa52a54019c6ee2b52903626dad4e

--- api/schema.py
+++ api/schema.py
@@ -5,7 +5,7 @@ from cerberus import Validator
 from .errors import BadInput
 
 USERNAME_REGEX = re.compile(r'^[a-z]{1}[a-zA-Z0-9_]{2,19}$', re.A)
-SUBDOMAIN_REGEX = re.compile(r'^[a-zA-Z0-9_-]{0,63}$', re.A)
+SUBDOMAIN_REGEX = re.compile(r'^([a-zA-Z0-9_][a-zA-Z0-9_-]{0,61}[a-zA-Z0-9_]|[a-zA-Z0-9_])$', re.A)
 DISCORD_REGEX = re.compile(r'^[^\#]{2,32}\#\d{4}$', re.A)
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', re.A)
 
