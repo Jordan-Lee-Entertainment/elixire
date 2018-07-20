6b545c337fa31abfb499195caf11dd7384c407e9
slice <ryaneft@gmail.com>
2018-07-20 11:04:39 -0700

    moar sql clean
    Former-commit-id: b1239f58a29b1c1e627a9ebd01991706f022499f

--- api/bp/upload/bp.py
+++ api/bp/upload/bp.py
@@ -118,11 +118,14 @@ def _construct_url(domain, shortname, extension):
 
 async def check_repeat(app, fspath: str, extension: str, ctx: UploadContext) -> Optional[Dict[str, Any]]:
     # check which files have the same fspath (the same hash)
-    files = await app.db.fetch("""
-    SELECT filename, uploader, domain
-    FROM files
-    WHERE fspath = $1 AND files.deleted = false
-    """, fspath)
+    files = await app.db.fetch(
+        """
+        SELECT filename, uploader, domain
+        FROM files
+        WHERE fspath = $1 AND files.deleted = false
+        """,
+        fspath
+    )
 
     # get the first file, if any, from the uploader
     try:
@@ -132,11 +135,14 @@ async def check_repeat(app, fspath: str, extension: str, ctx: UploadContext) -> 
         return
 
     # fetch domain info about that file
-    domain = await app.db.fetchval("""
-    SELECT domain
-    FROM domains
-    WHERE domain_id = $1
-    """, ufile['domain'])
+    domain = await app.db.fetchval(
+        """
+        SELECT domain
+        FROM domains
+        WHERE domain_id = $1
+        """,
+        ufile['domain']
+    )
 
     # use 'i' as subdomain by default
     # since files.subdomain isn't a thing.
