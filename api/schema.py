1b6dc51bffa15d2f7bc4976f8571046be44a7ec5
Luna Mendes <lkmnds@gmail.com>
2018-09-13 15:36:17 -0300

    Revert "api.schema: update USERNAME_REGEX, denying uppercase"
    This reverts commit e5fc6f20d8e3364c5333f16211b66e901f229840 [formerly 2b7eb95160bbc25bd36cd2372a2fe723f34b99cf].
    
    
    Former-commit-id: 0b9b8dd9b0e67a904416f9faa486ece582264ed4

--- api/schema.py
+++ api/schema.py
@@ -7,7 +7,7 @@ import re
 from cerberus import Validator
 from .errors import BadInput
 
-USERNAME_REGEX = re.compile(r'^[a-z0-9]{1}[a-z0-9_]{2,19}$', re.A)
+USERNAME_REGEX = re.compile(r'^[a-zA-Z0-9]{1}[a-zA-Z0-9_]{2,19}$', re.A)
 SUBDOMAIN_REGEX = re.compile(r'^([a-zA-Z0-9_][a-zA-Z0-9_-]{0,61}'
                              r'[a-zA-Z0-9_]|[a-zA-Z0-9_]|)$', re.A)
 DISCORD_REGEX = re.compile(r'^[^\#]{2,70}\#\d{4}$', re.A)
