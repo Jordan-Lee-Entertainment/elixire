550140a5971735ff63a91e8f643ddd63bda84095
Luna <git@l4.pm>
2018-12-23 18:00:34 -0300

    all: remove the gplv3 header
    preparing to move to SPDX IDS and AGPLv3.
    
    
    Former-commit-id: cc54d0eb64d5a22ade7e623ebafff3a157936420

--- api/common/__init__.py
+++ api/common/__init__.py
@@ -1,20 +1,3 @@
-"""
-elixire
-Copyright (C) 2018  elixi.re Team
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, version 3 of the License.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
-
 from .common import TokenType, FileNameType, get_ip_addr, \
     gen_filename, calculate_hash, delete_file, \
     delete_shorten, check_bans, get_domain_info, get_random_domain, \
