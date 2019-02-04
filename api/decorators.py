4fe025b98977ef3c06724cefc47545c130e841e3
slice <ryaneft@gmail.com>
2019-02-03 22:30:21 -0800

    api/decorators: use functools.wraps and better naming
    Add the usage of functools.wraps to avoid mangling the name of decorated
    functions. Also, it's just better practice.
    
    Generated wrapper functions also have better names in an attempt to keep
    the __repr__ of these functions more readable, but longer.
    
    
    Former-commit-id: 5cb2d1e64efb4513e16e7b43c386ee9de1bf68d0

--- api/decorators.py
+++ api/decorators.py
@@ -1,33 +1,35 @@
 # elixire: Image Host software
 # Copyright 2018, elixi.re Team and the elixire contributors
 # SPDX-License-Identifier: AGPL-3.0-only
-
-"""
-    elixire - route decorators
-"""
-
+import functools
 from .common.auth import token_check, check_admin
 
 
 def auth_route(handler):
     """Declare an authenticated route."""
-    async def _handler(request, *args, **kwargs):
+
+    @functools.wraps(handler)
+    async def auth_route_wrapped(request, *args, **kwargs):
         user_id = await token_check(request)
         return await handler(request, user_id, *args, **kwargs)
 
-    return _handler
+    return auth_route_wrapped
 
 
 def admin_route(handler):
     """Declare an admin route."""
-    async def _handler(request, *args, **kwargs):
+
+    @functools.wraps(handler)
+    async def admin_route_wrapped(request, *args, **kwargs):
         admin_id = await token_check(request)
 
         # raise exception on non-admins
         await check_admin(request, admin_id, True)
 
-        # if it is all good, call the old handler
+        # inject admin id into the request to be used later
         request['_ctx_admin_id'] = admin_id
+
+        # if it is all good, call the old handler
         return await handler(request, admin_id, *args, **kwargs)
 
-    return _handler
+    return admin_route_wrapped
