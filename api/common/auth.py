3967c01e542e4d2b68586c70795bce3bbf742b83
Luna <git@l4.pm>
2019-04-18 16:08:37 -0300

    auth: call pwd_check before active check
    Former-commit-id: 5d4ec280db517f27fc3eecb79a70053b278edc40

--- api/common/auth.py
+++ api/common/auth.py
@@ -180,12 +180,13 @@ async def login_user(request):
         log.info(f'login: {username!r} does not exist')
         raise FailedAuth('User or password invalid')
 
+    await pwd_check(request, user['password_hash'], password)
+
     if not user['active']:
         log.warning(f'login: {username!r} is not active')
         raise FailedAuth('User is deactivated')
 
     await check_bans(request, user['user_id'])
-    await pwd_check(request, user['password_hash'], password)
 
     return user
 
