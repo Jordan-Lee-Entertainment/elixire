61e58944579c2db5c10dbbed14bde443fe123efe
Luna <git@l4.pm>
2019-05-27 17:03:00 -0300

    wpadmin: add GET /api/science, GET /api/boron
    Former-commit-id: 67bf84487d1d7a1342d464f9eb1a2f429853a352

--- api/bp/wpadmin.py
+++ api/bp/wpadmin.py
@@ -2,15 +2,19 @@
 # Copyright 2018-2019, elixi.re Team and the elixire contributors
 # SPDX-License-Identifier: AGPL-3.0-only
 
+# This blueprint contains routes for the sole purpose of being humorous.
+
+import datetime
 import logging
 import random
 
 from sanic import Blueprint
 from sanic import response
 
-
 bp = Blueprint('wpadmin')
 log = logging.getLogger(__name__)
+
+WORLD_POWER_DEADLINE = datetime.date(2023, 7, 24)
 
 # Inspired by:
 # https://gist.github.com/NickCraver/c9458f2e007e9df2bdf03f8a02af1d13
@@ -40,3 +44,23 @@ async def wpadmin(request):
     url = random.choice(memes)
 
     return response.redirect(url)
+
+
+@bp.get('/api/science')
+async def science_route(request):
+    """*insert b4nzyblob*"""
+    return response.text("Hewoo! We'we nyot discowd we don't spy on nyou :3")
+
+
+@bp.get('/api/boron')
+async def ipek_yolu(request):
+    """calculates days until 100th year anniversary of treaty of lausanne"""
+    today = datetime.date.today()
+    days_to_wp = (WORLD_POWER_DEADLINE - today).days
+    is_world_power = (days_to_wp <= 0)
+
+    return response.json({
+        'world_power': is_world_power,
+        'days_until_world_power': days_to_wp
+    })
+
