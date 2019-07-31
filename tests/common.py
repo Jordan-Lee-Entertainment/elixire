87a06a66106b9c7341c6f19e49f859e73e7ad62e
Luna <git@l4.pm>
2019-07-31 15:58:00 -0300

    copy TestClient and basic test fixture from litecord

--- tests/common.py
+++ tests/common.py
@@ -8,8 +8,6 @@ import io
 import base64
 import string
 
-from .creds import USERNAME, PASSWORD, ADMIN_USER, ADMIN_PASSWORD
-
 EMAIL_ALPHABET = string.ascii_lowercase
 
 
@@ -64,3 +62,48 @@ async def login_admin(test_cli) -> str:
     assert isinstance(data, dict)
 
     return data["token"]
+
+
+class TestClient:
+    """Test client that wraps quart's TestClient and a test
+    user and adds authorization headers to test requests."""
+
+    def __init__(self, test_cli, test_user):
+        self.cli = test_cli
+        self.app = test_cli.app
+        self.user = test_user
+
+    def __getitem__(self, key):
+        return self.user[key]
+
+    def _inject_auth(self, kwargs: dict) -> list:
+        """Inject the test user's API key into the test request before
+        passing the request on to the underlying TestClient."""
+        headers = kwargs.get("headers", {})
+        headers["authorization"] = self.user["token"]
+        return headers
+
+    async def get(self, *args, **kwargs):
+        """Send a GET request."""
+        kwargs["headers"] = self._inject_auth(kwargs)
+        return await self.cli.get(*args, **kwargs)
+
+    async def post(self, *args, **kwargs):
+        """Send a POST request."""
+        kwargs["headers"] = self._inject_auth(kwargs)
+        return await self.cli.post(*args, **kwargs)
+
+    async def put(self, *args, **kwargs):
+        """Send a POST request."""
+        kwargs["headers"] = self._inject_auth(kwargs)
+        return await self.cli.put(*args, **kwargs)
+
+    async def patch(self, *args, **kwargs):
+        """Send a PATCH request."""
+        kwargs["headers"] = self._inject_auth(kwargs)
+        return await self.cli.patch(*args, **kwargs)
+
+    async def delete(self, *args, **kwargs):
+        """Send a DELETE request."""
+        kwargs["headers"] = self._inject_auth(kwargs)
+        return await self.cli.delete(*args, **kwargs)
