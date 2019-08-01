30d01886aecc2a481b889eba8a1244da576aa306
Luna <git@l4.pm>
2019-08-01 09:47:04 -0300

    tests.test_upload: use test_cli_user

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -4,12 +4,12 @@
 
 import pytest
 import secrets
-from .common import login_normal, png_data
+from .common import png_data
 
 
-async def check_exists(test_cli, shortname, utoken, not_exists=False):
+async def check_exists(test_cli, shortname, not_exists=False):
     """Check if a file exists, given the shortname, token, etc."""
-    resp = await test_cli.get("/api/list?page=0", headers={"Authorization": utoken})
+    resp = await test_cli.get("/api/list?page=0")
 
     assert resp.status_code == 200
     rjson = await resp.json
@@ -22,13 +22,11 @@ async def check_exists(test_cli, shortname, utoken, not_exists=False):
         assert shortname in rjson["files"]
 
 
-async def _test_upload_png(test_cli):
+async def _test_upload_png(test_cli_user):
     """Test that the upload route works given test data"""
-    utoken = await login_normal(test_cli)
-
     # file uploads not available yet.
     # https://gitlab.com/pgjones/quart/issues/147
-    resp = await test_cli.post(
+    resp = await test_cli_user.post(
         "/api/upload",
         headers={"authorization": utoken, "content-type": "multipart/form-data"},
         form={"file": (png_data(), "random.png"), "content-type": "image/png"},
@@ -39,49 +37,36 @@ async def _test_upload_png(test_cli):
     assert isinstance(respjson, dict)
     assert isinstance(respjson["url"], str)
     assert isinstance(respjson["delete_url"], str)
-    await check_exists(test_cli, respjson["shortname"], utoken)
+    await check_exists(test_cli_user, respjson["shortname"], utoken)
 
 
 @pytest.mark.asyncio
-async def _test_delete_file(test_cli):
-    utoken = await login_normal(test_cli)
-
+async def _test_delete_file(test_cli_user):
     # TODO file
-    resp = await test_cli.post("/api/upload", headers={"Authorization": utoken})
+    resp = await test_cli_user.post("/api/upload", headers={"Authorization": utoken})
 
     assert resp.status_code == 200
     respjson = await resp.json
     assert isinstance(respjson, dict)
     assert isinstance(respjson["url"], str)
-    await check_exists(test_cli, respjson["shortname"], utoken)
+    await check_exists(test_cli_user, respjson["shortname"])
 
     # test delete
     short = respjson["shortname"]
 
-    resp_del = await test_cli.delete(
-        f"/api/files/{short}", headers={"Authorization": utoken}
-    )
-
+    resp_del = await test_cli_user.delete(f"/api/files/{short}")
     assert resp_del.status_code == 204
-
-    await check_exists(test_cli, respjson["shortname"], utoken, True)
+    await check_exists(test_cli_user, respjson["shortname"], True)
 
 
 @pytest.mark.asyncio
-async def test_delete_nonexist(test_cli):
+async def test_delete_nonexist(test_cli_user):
     """Test deletions of files that don't exist."""
-    utoken = await login_normal(test_cli)
-    rand_file = secrets.token_urlsafe(20)
+    rand_file = secrets.token_hex(20)
 
-    resp_del = await test_cli.delete(
-        f"/api/files/{rand_file}", headers={"Authorization": utoken}
-    )
-
+    resp_del = await test_cli_user.delete(f"/api/files/{rand_file}")
     assert resp_del.status_code == 404
 
     # ensure sharex compatibility endpoint works too
-    resp_del = await test_cli.get(
-        f"/api/files/{rand_file}/delete", headers={"Authorization": utoken}
-    )
-
+    resp_del = await test_cli_user.get(f"/api/files/{rand_file}/delete")
     assert resp_del.status_code == 404
