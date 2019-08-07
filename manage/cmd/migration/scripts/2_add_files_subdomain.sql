67acf9e07c3cf19a22bd4ff630cebcf26484c9cc
Luna <git@l4.pm>
2019-08-07 11:00:01 -0300

    migration: change default/v2 sentinel subdomain column values

--- manage/cmd/migration/scripts/2_add_files_subdomain.sql
+++ manage/cmd/migration/scripts/2_add_files_subdomain.sql
@@ -1,16 +1,16 @@
 ALTER TABLE files
-    ADD COLUMN subdomain text DEFAULT NULL;
+    ADD COLUMN subdomain text DEFAULT "";
 
 -- files are now only accessible on the subdomain that they were uploaded on,
 -- however files that were updated before this change need to stay accessible
 -- on both the root domain and any subdomain. this is signalled using an empty
 -- string for the subdomain.
 UPDATE files
-    SET subdomain = '';
+    SET subdomain = NULL;
 
 ALTER TABLE shortens
-    ADD COLUMN subdomain text DEFAULT NULL;
+    ADD COLUMN subdomain text DEFAULT "";
 
 -- (see above comment)
 UPDATE shortens
-    SET subdomain = '';
+    SET subdomain = NULL;
