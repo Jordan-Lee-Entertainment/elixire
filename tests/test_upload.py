6e590b6ba2c738c92313ca7d72abd1dee660a064
slice <ryaneft@gmail.com>
2019-08-14 18:49:15 -0700

    test_upload_png: test on random domain
    * also split up the original test into test_delete_file as well, since
      they seem to be testing two different things.

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -43,7 +43,7 @@ async def check_exists(test_cli, shortname, *, reverse=False):
     assert resp.status_code == 404
 
 
-async def test_upload_png(test_cli_user):
+async def test_upload_png(test_cli_user, test_domain):
     """Test that the upload route works given test data"""
     # file uploads aren't natively available under QuartClient, see:
     # https://gitlab.com/pgjones/quart/issues/147
@@ -53,14 +53,44 @@ async def test_upload_png(test_cli_user):
 
     # TODO set to some random domain_id and subdomain for nicer testing
 
-    resp = await test_cli_user.post("/api/upload", **(await png_request()))
+    subdomain = hexs(10)
+    resp = await test_cli_user.post(
+        "/api/upload",
+        **(await png_request()),
+        query_string={"domain": test_domain.id, "subdomain": subdomain},
+    )
 
     assert resp.status_code == 200
-    respjson = await resp.json
-    assert isinstance(respjson, dict)
-    assert isinstance(respjson["url"], str)
-    await check_exists(test_cli_user, respjson["shortname"])
+    json = await resp.json
+    shortname = json["shortname"]
+    assert isinstance(json, dict)
+    assert isinstance(json["url"], str)
+    await check_exists(test_cli_user, shortname)
 
+    url = f"/i/{shortname}.png"
+
+    host_without_wildcard = test_domain.name.replace("*.", "")
+    # (browsers won't be accessing "*.domain.tld", they will access "domain.tld"
+    # -- "*.domain.tld" is how they are represented internally)
+
+    # accessing on the subdomain+domain that it was uploaded to works
+    resp = await test_cli_user.get(
+        url, headers={"host": f"{subdomain}.{host_without_wildcard}"}
+    )
+    assert resp.status_code == 200
+
+    # accessing on the base domain without a subdomain doesn't work
+    resp = await test_cli_user.get(url, headers={"host": host_without_wildcard})
+    assert resp.status_code == 404
+
+    # accessing on the domain but with a different subdomain doesn't work
+    resp = await test_cli_user.get(
+        url, headers={"host": f"nope.{host_without_wildcard}"}
+    )
+    assert resp.status_code == 404
+
+
+async def test_delete_file(test_cli_user):
     resp = await test_cli_user.post("/api/upload", **(await png_request()))
 
     assert resp.status_code == 200
