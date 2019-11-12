349cfba733eb82991b22118cc46d9859879c4e6b
Luna <git@l4.pm>
2019-11-12 15:55:36 -0300

    register: use REQUIRE_APPROVAL, return it in json
     - config.ci.py: black fmt run

--- config.ci.py
+++ config.ci.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
 
-MAIN_URL = 'http://localhost:8000'
+MAIN_URL = "http://localhost:8000"
 
 USE_HTTPS = False
 
@@ -11,23 +11,15 @@ PORT = 8000
 
 ENABLE_FRONTEND = False
 
-db = {
-   "host": "postgres",
-   "user": "postgres",
-   "password": ""
-}
+db = {"host": "postgres", "user": "postgres", "password": ""}
 
 redis = "redis://redis"
-TOKEN_SECRET = b':\xed\x9d\x19\x1b\xf5\xed\x02\xba12\x97$W\xb7\x9a\x97\xc2\xaf\xa8\xc3\x86\xeeR\xf1lg.\xacWR\x91\xaf|\xcf\xff\xc8\xb0\xccF\x83\x06`\xa7p\xf5B\xb5'
+TOKEN_SECRET = b":\xed\x9d\x19\x1b\xf5\xed\x02\xba12\x97$W\xb7\x9a\x97\xc2\xaf\xa8\xc3\x86\xeeR\xf1lg.\xacWR\x91\xaf|\xcf\xff\xc8\xb0\xccF\x83\x06`\xa7p\xf5B\xb5"
 
 RATELIMITS = {
     # global ratelimit for authenticated connections
-    '/': {
-        'requests': 10000,
-        'second': 1
-    },
-    ('ip', '/'): {
-        'requests': 10000,
-        'second': 1
-    },
+    "/": {"requests": 10000, "second": 1},
+    ("ip", "/"): {"requests": 10000, "second": 1},
 }
+
+REQUIRE_APPROVALS = False
