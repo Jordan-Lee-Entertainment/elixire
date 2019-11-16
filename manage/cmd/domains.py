552ba1d28b3b145a18c82b5e991be36e04866fb8
Luna <git@l4.pm>
2019-11-16 16:21:24 -0300

    manage.cmd.domains: add domain tag adding/removal

--- manage/cmd/domains.py
+++ manage/cmd/domains.py
@@ -2,7 +2,12 @@
 # Copyright 2018-2019, elixi.re Team and the elixire contributors
 # SPDX-License-Identifier: AGPL-3.0-only
 
-from api.common.domain import create_domain_tag, delete_domain_tag
+from api.common.domain import (
+    create_domain_tag,
+    delete_domain_tag,
+    add_domain_tag,
+    remove_domain_tag,
+)
 
 
 async def create_tag(_ctx, args):
@@ -15,11 +20,37 @@ async def delete_tag(_ctx, args):
     print("OK")
 
 
+async def add_tag(_ctx, args):
+    await add_domain_tag(args.domain_id, args.tag_id)
+    print("OK")
+
+
+async def remove_tag(_ctx, args):
+    await remove_domain_tag(args.domain_id, args.tag_id)
+    print("OK")
+
+
 def setup(subparser):
     parser_create_tag = subparser.add_parser("create_tag", help="Create a domain tag")
     parser_create_tag.add_argument("label")
     parser_create_tag.set_defaults(func=create_tag)
 
     parser_delete_tag = subparser.add_parser("delete_tag", help="Delete a domain tag")
-    parser_delete_tag.add_argument("tag_id")
+    parser_delete_tag.add_argument("tag_id", type=int)
     parser_delete_tag.set_defaults(func=delete_tag)
+
+    parser_delete_tag = subparser.add_parser(
+        "add_tag", help="Add a domain tag to a domain"
+    )
+    parser_delete_tag.add_argument("domain_id", type=int)
+    parser_delete_tag.add_argument("tag_id", type=int)
+    parser_delete_tag.set_defaults(func=add_tag)
+
+    parser_delete_tag = subparser.add_parser(
+        "remove_tag",
+        help="Remove a domain tag to a domain",
+        description="If the tag isn't in the domain, this still says OK",
+    )
+    parser_delete_tag.add_argument("domain_id", type=int)
+    parser_delete_tag.add_argument("tag_id", type=int)
+    parser_delete_tag.set_defaults(func=remove_tag)
