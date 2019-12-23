9a864983b00a99c2102e950871458221b8caa411
Luna <git@l4.pm>
2019-12-23 14:29:04 -0300

    test_datadump: add asserts for listing + fix job state fetch

--- tests/test_datadump.py
+++ tests/test_datadump.py
@@ -13,20 +13,32 @@ log = logging.getLogger(__name__)
 
 
 async def test_profile_work(test_cli_user):
-    resp = await test_cli_user.post("/api/dump/request")
+    resp = await test_cli_user.post("/api/dump")
     assert resp.status_code == 200
 
     try:
         rjson = await resp.json
         assert isinstance(rjson, dict)
         assert isinstance(rjson["job_id"], str)
+        job_id = rjson["job_id"]
         filename = f"{test_cli_user['user_id']}_{test_cli_user['username']}.zip"
+
+        resp = await test_cli_user.get("/api/dump")
+        assert resp.status_code == 200
+        rjson = await resp.json
+        assert isinstance(rjson, dict)
+        assert isinstance(rjson["results"], list)
+        assert rjson["results"]
+        assert rjson["results"][0]["job_id"] == job_id
+        assert isinstance(rjson["pagination"], dict)
+        assert "total" in rjson["pagination"]
+        assert "current" in rjson["pagination"]
 
         # we can't really wait for the job to complete here, so instead
         # we sleep()
         await asyncio.sleep(1.3)
 
-        status = await test_cli_user.app.sched.fetch_queue_job_status(rjson["job_id"])
+        status = await test_cli_user.app.sched.fetch_queue_job_status(job_id)
         assert status.queue_name == "datadump"
         assert not status.errors
     finally:
