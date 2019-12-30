798015a29b67108aeec253a00edf62e25d05e315
Luna <git@l4.pm>
2019-12-29 22:58:09 -0300

    audit_log_actions.user: use User

--- api/bp/admin/audit_log_actions/user.py
+++ api/bp/admin/audit_log_actions/user.py
@@ -2,49 +2,25 @@
 # Copyright 2018-2019, elixi.re Team and the elixire contributors
 # SPDX-License-Identifier: AGPL-3.0-only
 import logging
-from typing import Dict, Any
 
-from quart import current_app as app
-from api.bp.personal_stats import get_counts
 from api.bp.admin.audit_log import EditAction, DeleteAction
+from api.common.profile import get_counts
 from api.models import User
 
 log = logging.getLogger(__name__)
 
 
-async def get_user(user_id) -> dict:
-    """Get a user as a dictionary."""
-    user = await User.fetch(user_id)
-    if user is None:
-        return {}
-
-    duser: Dict[str, Any] = {}
-    for field in User.__slots__:
-        duser[field] = getattr(duser, field)
-
-    limits = await app.db.fetchrow(
-        """
-        SELECT blimit, shlimit
-        FROM limits
-        WHERE user_id = $1
-        """,
-        user_id,
-    )
-
-    assert limits is not None
-    dlimits = dict(limits)
-    return {**duser, **dlimits}
-
-
 class UserEditAction(EditAction):
-    async def get_object(self, user_id):
-        return await get_user(user_id)
+    async def get_object(self, user_id: int) -> dict:
+        user = await User.fetch(user_id)
+        assert user is not None
+        return user.to_dict()
 
     async def details(self):
         if not self.different_keys():
             return False
 
-        lines = [f'User {self.after["username"]} ({self.id}) was edited.']
+        lines = [f'User {self.after["name"]} ({self.id}) was edited.']
 
         for key, old, new in self.different_keys_items():
             lines.append(f"\t - {key}: {old} => {new}")
@@ -53,10 +29,12 @@ class UserEditAction(EditAction):
 
 
 class UserDeleteAction(DeleteAction):
-    async def get_object(self, user_id):
-        user = await get_user(user_id)
-        user.update(await get_counts(self.app.db, user_id))
-        return user
+    async def get_object(self, user_id: int) -> dict:
+        user = await User.fetch(user_id)
+        assert user is not None
+
+        # TODO add counts to User, remove need for get_counts
+        return {**user.to_dict(), **await get_counts(user_id)}
 
     async def details(self) -> list:
         lines = [f"User ID {self.id} was deleted", "Domain information:"]
