b57c65a91a013854328abd5fa6a263ad4b104dd0
Luna <git@l4.pm>
2019-12-29 22:04:47 -0300

    audit_log_actions.user: use User.fetch

--- api/bp/admin/audit_log_actions/user.py
+++ api/bp/admin/audit_log_actions/user.py
@@ -2,48 +2,43 @@
 # Copyright 2018-2019, elixi.re Team and the elixire contributors
 # SPDX-License-Identifier: AGPL-3.0-only
 import logging
+from typing import Dict, Any
 
+from quart import current_app as app
 from api.bp.personal_stats import get_counts
 from api.bp.admin.audit_log import EditAction, DeleteAction
+from api.models import User
 
 log = logging.getLogger(__name__)
 
 
-async def get_user(conn, user_id) -> dict:
+async def get_user(user_id) -> dict:
     """Get a user as a dictionary."""
-    user = await conn.fetchrow(
-        """
-    SELECT username, active, email, consented, admin, paranoid,
-        subdomain, domain,
-        shorten_subdomain, shorten_domain
-    FROM users
-    WHERE user_id = $1
-    """,
-        user_id,
-    )
-
+    user = await User.fetch(user_id)
     if user is None:
         return {}
 
-    duser = dict(user)
+    duser: Dict[str, Any] = {}
+    for field in User.__slots__:
+        duser[field] = getattr(duser, field)
 
-    limits = await conn.fetchrow(
+    limits = await app.db.fetchrow(
         """
-    SELECT blimit, shlimit
-    FROM limits
-    WHERE user_id = $1
-    """,
+        SELECT blimit, shlimit
+        FROM limits
+        WHERE user_id = $1
+        """,
         user_id,
     )
 
+    assert limits is not None
     dlimits = dict(limits)
-
     return {**duser, **dlimits}
 
 
 class UserEditAction(EditAction):
     async def get_object(self, user_id):
-        return await get_user(self.app.db, user_id)
+        return await get_user(user_id)
 
     async def details(self):
         if not self.different_keys():
@@ -59,7 +54,7 @@ class UserEditAction(EditAction):
 
 class UserDeleteAction(DeleteAction):
     async def get_object(self, user_id):
-        user = await get_user(self.app.db, user_id)
+        user = await get_user(user_id)
         user.update(await get_counts(self.app.db, user_id))
         return user
 
