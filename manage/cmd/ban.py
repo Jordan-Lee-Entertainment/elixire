06f5e8fb856473b802ef57682b93d6cbcba46bdf
Luna <git@l4.pm>
2020-01-02 00:35:11 -0300

    manage.cmd.ban: fix redis invalidation

--- manage/cmd/ban.py
+++ manage/cmd/ban.py
@@ -7,7 +7,7 @@ from ..utils import get_user
 
 
 async def _invalidate(ctx, user_id: int):
-    await ctx.storage.invalidate(f'userban:{user_id}')
+    await ctx.storage.raw_invalidate(f'userban:{user_id}')
 
 
 async def ban_user(ctx, args):
@@ -30,12 +30,12 @@ async def unban_user(ctx, args):
     """Unban a single user"""
     username = args.username
     user_id = await get_user(ctx, username)
-    await _invalidate(ctx, user_id)
 
     exec_out = await ctx.db.execute("""
     DELETE FROM bans
     WHERE user_id = $1
     """, user_id)
+    await _invalidate(ctx, user_id)
 
     print(f'SQL result: {exec_out}')
 
