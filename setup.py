06eb3d6557340520fe0badc544964d4301b68101
Luna <508270-luna@users.noreply.gitlab.com>
2020-01-06 08:40:19 +0000

    v3: update copyright year

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 # elixire: Image Host software
-# Copyright 2018-2019, elixi.re Team and the elixire contributors
+# Copyright 2018-2020, elixi.re Team and the elixire contributors
 # SPDX-License-Identifier: AGPL-3.0-only
 
 from setuptools import setup
 
 setup(
     name="elixire",
-    version="2018-2019.2.0.0",
+    version="2018-2020.2.0.0",
     description="Image host",
     url="https://elixi.re",
     author="Ave Ozkal, Luna Mendes, Mary Strodl, slice",
