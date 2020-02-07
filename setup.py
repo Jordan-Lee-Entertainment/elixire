b30c09b2943782bc7170b2907b380a7e56587709
Luna <508270-luna@users.noreply.gitlab.com>
2020-02-07 19:08:10 +0000

    gitlab-ci: remove pipenv
     - move dev-requirements.txt to tox.ini deps section

--- setup.py
+++ setup.py
@@ -10,5 +10,29 @@ setup(
     description="Image host",
     url="https://elixi.re",
     author="Ave Ozkal, Luna Mendes, Mary Strodl, slice",
-    python_requires=">=3.6",
+    python_requires=">=3.7",
+    install_requires=[
+        "hypercorn==0.9.0",
+        "bcrypt==3.1.7",
+        "itsdangerous==1.1.0",
+        "cerberus==1.3.2",
+        # --------------------------------------------------------------------
+        # when changing these dependencies, make sure to change them in
+        # .gitlab-ci.yml, too.
+        "aioredis==1.3.1",
+        "asyncpg==0.20.1",
+        # ---------------------------------------------------------------------
+        "aiohttp==3.6.2",
+        "aioinflux==0.9.0",
+        "cryptography==2.8",
+        "python-magic==0.4.15",
+        "parsedatetime==2.5",
+        "dnspython==1.16.0",
+        "Quart==0.10.0",
+        "Pillow==7.0.0",
+        "python-dateutil==2.8.1",
+        "violet @ git+https://gitlab.com/elixire/violet.git@9c6ad23c5fb436acfc4dd1b7caaa0c44754b57e6#egg=violet",
+        "winter @ git+https://gitlab.com/elixire/winter.git@988c6ca438663c30c6b617bdb16fd6be6c4226ba#egg=winter",
+        "hail @ git+https://gitlab.com/elixire/hail.git@d72895019ef68eb96bb775f939182dd9344de36#egg=hail",
+    ],
 )
