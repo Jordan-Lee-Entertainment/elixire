dbc6b8b6bb7f2bc590ff95fca5342f6abf0a3881
Luna <git@l4.pm>
2020-02-13 22:49:15 -0300

    models: add Domain.create, delete, and tag methods

--- api/models/domain.py
+++ api/models/domain.py
@@ -153,3 +153,204 @@ class Domain:
             return None
 
         return await User.fetch(owner_id)
+
+    @classmethod
+    async def create(
+        _,
+        name: str,
+        *,
+        tags: Optional[List[int]] = None,
+        permissions: int = 3,
+        owner_id: Optional[int] = None,
+    ) -> "Domain":
+        tags = tags or []
+
+        async with app.db.acquire() as conn:
+            async with conn.transaction():
+
+                domain_id = await app.db.fetchval(
+                    """
+                    INSERT INTO domains
+                        (domain, permissions)
+                    VALUES
+                        ($1, $2)
+                    RETURNING domain_id
+                    """,
+                    name,
+                    permissions,
+                )
+
+                domain = Domain(
+                    {
+                        "domain_id": domain_id,
+                        "domain": name,
+                        "permissions": permissions,
+                    },
+                    tags=Tags([]),
+                )
+
+                if owner_id:
+                    await domain.set_owner(owner_id)
+
+                for tag_id in tags:
+                    await domain.add_tag(tag_id)
+
+        # invalidate cache
+        possibilities = solve_domain(domain.domain)
+        await app.storage.raw_invalidate(*possibilities)
+
+        return domain
+
+    async def delete(self) -> dict:
+        """Delete a domain.
+
+        The related cache keys will be invalidated for you.
+        """
+        if self.id == 0:
+            raise BadInput("The root domain can not be deleted")
+
+        domain_name = await app.db.fetchval(
+            """
+            SELECT domain
+            FROM domains
+            WHERE domain_id = $1
+            """,
+            self.id,
+        )
+
+        files_count = await app.db.execute(
+            """
+            UPDATE files set domain = 0 WHERE domain = $1
+            """,
+            self.id,
+        )
+
+        shorten_count = await app.db.execute(
+            """
+            UPDATE shortens set domain = 0 WHERE domain = $1
+            """,
+            self.id,
+        )
+
+        users_count = await app.db.execute(
+            """
+            UPDATE users set domain = 0 WHERE domain = $1
+            """,
+            self.id,
+        )
+
+        users_shorten_count = await app.db.execute(
+            """
+            UPDATE users set shorten_domain = 0 WHERE shorten_domain = $1
+            """,
+            self.id,
+        )
+
+        await app.db.execute(
+            """
+            DELETE FROM domain_owners
+            WHERE domain_id = $1
+            """,
+            self.id,
+        )
+
+        result = await app.db.execute(
+            """
+            DELETE FROM domains
+            WHERE domain_id = $1
+            """,
+            self.id,
+        )
+
+        # invalidate cache
+        keys = solve_domain(domain_name)
+        await app.storage.raw_invalidate(*keys)
+
+        # TODO make this a namedtuple
+        return {
+            "file_move_result": files_count,
+            "shorten_move_result": shorten_count,
+            "users_move_result": users_count,
+            "users_shorten_move_result": users_shorten_count,
+            "result": result,
+        }
+
+    async def set_owner(self, owner_id: int) -> None:
+        """Set domain owner for the given domain."""
+        await app.db.execute(
+            """
+            INSERT INTO domain_owners (domain_id, user_id)
+            VALUES ($1, $2)
+            ON CONFLICT ON CONSTRAINT domain_owners_pkey
+            DO UPDATE
+                SET user_id = $2
+                WHERE domain_owners.domain_id = $1
+            """,
+            self.id,
+            owner_id,
+        )
+
+    async def add_tag(self, tag_id: int) -> None:
+        """Add a tag to a domain.
+
+        Keep in mind to refetch your model on update.
+        """
+        try:
+            await app.db.execute(
+                """
+                INSERT INTO domain_tag_mappings
+                    (domain_id, tag_id)
+                VALUES
+                    ($1, $2)
+                """,
+                self.id,
+                tag_id,
+            )
+        except UniqueViolationError:
+            pass
+
+    async def remove_domain_tag(self, tag_id: int) -> None:
+        """Remove a tag from a domain.
+
+        Keep in mind to refetch your model on update.
+        """
+
+        await app.db.execute(
+            """
+            DELETE FROM domain_tag_mappings
+            WHERE domain_id = $1 AND tag_id = $2
+            """,
+            self.id,
+            tag_id,
+        )
+
+    async def set_domain_tags(self, tags: Tags) -> None:
+        """Set tags for a given domain and delete the previously assigned ones.
+
+        Updates the model.
+        """
+        existing_set = {tag.id for tag in self.tags}
+        tags_set = set(tags)
+
+        to_add = tags_set - existing_set
+        to_remove = existing_set - tags_set
+
+        async with app.db.acquire() as conn:
+            async with conn.transaction():
+                await app.db.executemany(
+                    """
+                    INSERT INTO domain_tag_mappings (domain_id, tag_id)
+                    VALUES ($1, $2)
+                    """,
+                    [(self.id, tag_id) for tag_id in to_add],
+                )
+
+                await app.db.executemany(
+                    """
+                    DELETE FROM domain_tag_mappings
+                    WHERE domain_id = $1 AND tag_id = $2
+                    """,
+                    [(self.id, tag_id) for tag_id in to_remove],
+                )
+
+        self.tags = tags
