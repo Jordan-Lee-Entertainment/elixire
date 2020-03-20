b09e76b17229da12b3b77553de7a2c007a478fae
Luna <git@l4.pm>
2020-03-19 21:41:56 -0300

    bump violet dep

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         "Quart==0.10.0",
         "Pillow==7.0.0",
         "python-dateutil==2.8.1",
-        "violet @ git+https://gitlab.com/elixire/violet.git@11b870decd1efba5e2e4ea5765d4ac20a3b9a38f#egg=violet",
+        "violet @ git+https://gitlab.com/elixire/violet.git@49252304b98ddace6a31fd589c0a48be067a7e60#egg=violet",
         "winter @ git+https://gitlab.com/elixire/winter.git@988c6ca438663c30c6b617bdb16fd6be6c4226ba#egg=winter",
         "hail @ git+https://gitlab.com/elixire/hail.git@d72895019ef68eb96bb775f939182dd9344de36#egg=hail",
     ],
