02e7b29f2ff0c1d8d23ba24d2636559ed4a58334
Luna <git@l4.pm>
2020-03-25 21:30:35 -0300

    test_manage: add sendmail test
    This requires us to peek at the state of the app that is inside the
    manage main function (since it is fully mocked)

--- tests/test_manage.py
+++ tests/test_manage.py
@@ -5,9 +5,30 @@ import pytest
 
 from manage.main import amain
 
+from tests.common.generators import username
+
 pytestmark = pytest.mark.asyncio
 
 
+async def _run(test_cli_user, args):
+    return await amain(
+        test_cli_user.app.loop, test_cli_user.app.econfig, args, test=True
+    )
+
+
 async def test_help(event_loop, app):
-    status = await amain(event_loop, app.econfig, [])
+    _, status = await amain(event_loop, app.econfig, [])
     assert status == 0
+
+
+async def test_sendmail(test_cli_user):
+    subject, body = username(), username()
+
+    app, status = await _run(
+        test_cli_user, ["sendmail", test_cli_user.user["username"], subject, body]
+    )
+    assert status == 0
+
+    email = app._email_list[-1]
+    assert email["subject"] == subject
+    assert email["content"] == body
