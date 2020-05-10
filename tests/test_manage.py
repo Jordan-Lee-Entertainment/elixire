d0e3577a87e7e4e7c4c62f53bcde12f8eeb9ff83
Luna <git@l4.pm>
2020-05-09 21:36:00 -0300

    Add tests for rename/delete file ops

--- tests/test_manage.py
+++ tests/test_manage.py
@@ -5,7 +5,8 @@ import pytest
 
 from manage.main import amain
 
-from tests.common.generators import username
+from tests.common.generators import username, png_data
+from api.models import File
 
 pytestmark = pytest.mark.asyncio
 
@@ -42,3 +43,24 @@ async def test_find_inactive(test_cli_user):
 async def test_find_unused(test_cli_user):
     app, status = await _run(test_cli_user, ["find_unused"])
     assert status == 0
+
+
+async def test_file_operations(test_cli_user):
+    sent_file = await test_cli_user.create_file("test.png", png_data(), "image/png")
+    wanted_shortname = username()
+
+    app, status = await _run(
+        test_cli_user, ["rename_file", sent_file.shortname, wanted_shortname]
+    )
+    assert status == 0
+
+    async with test_cli_user.app.app_context():
+        wanted_file = await File.fetch_by(shortname=wanted_shortname)
+        assert wanted_file is not None
+
+    app, status = await _run(test_cli_user, ["delete", wanted_shortname])
+    assert status == 0
+
+    async with test_cli_user.app.app_context():
+        wanted_file = await File.fetch_by(shortname=wanted_shortname)
+        assert wanted_file.deleted
