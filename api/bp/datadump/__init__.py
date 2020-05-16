4a4d7755d52dc50636fdce761f4121f8913f38be
Luna <git@l4.pm>
2020-05-15 21:48:09 -0300

    fix imports

--- api/bp/datadump/__init__.py
+++ api/bp/datadump/__init__.py
@@ -2,7 +2,7 @@
 # Copyright 2018-2020, elixi.re Team and the elixire contributors
 # SPDX-License-Identifier: AGPL-3.0-only
 
-from .handler import handler
+from .handler import DatadumpQueue
 from .bp import bp, start
 
-__all__ = ["handler", "bp", "start"]
+__all__ = ["DatadumpQueue", "bp", "start"]
