e565f0da57afdbbd4f110e6041834afccd7e5be2
Luna <git@l4.pm>
2020-05-15 22:10:02 -0300

    fix test to use job queue classmethods

--- tests/test_datadump.py
+++ tests/test_datadump.py
@@ -12,6 +12,7 @@ from urllib.parse import parse_qs
 from tests.common.utils import extract_first_url
 from tests.common.generators import png_data
 from api.bp.datadump.janitor import dump_janitor
+from api.bp.datadump import DatadumpQueue
 
 pytestmark = pytest.mark.asyncio
 
@@ -57,9 +58,9 @@ async def test_datadump(test_cli_user):
         assert dump["state"] == "processing"
         assert dump["job_id"] == job_id
 
-        await test_cli_user.app.sched.wait_job(job_id, timeout=20)
+        await DatadumpQueue.wait_job(job_id, timeout=20)
 
-        status = await test_cli_user.app.sched.fetch_queue_job_status(job_id)
+        status = await DatadumpQueue.fetch_job_status(job_id)
         assert status.queue_name == "datadump"
         assert not status.errors
 
