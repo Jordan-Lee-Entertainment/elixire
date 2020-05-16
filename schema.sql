8e312584149b7389a70cc31dc0f2b3f0ff8f354f
Luna <git@l4.pm>
2020-05-15 23:26:32 -0300

    add mass_delete_queue psql table

--- schema.sql
+++ schema.sql
@@ -223,3 +223,18 @@ CREATE TABLE IF NOT EXISTS datadump_queue (
 
     user_id bigint REFERENCES users (user_id) ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS mass_delete_queue (
+    job_id uuid primary key,
+    name text unique,
+
+    state bigint default 0,
+    errors text default '',
+    inserted_at timestamp without time zone default (now() at time zone 'utc'),
+    scheduled_at timestamp without time zone default (now() at time zone 'utc'),
+    taken_at timestamp without time zone default null,
+    internal_state jsonb default '{}',
+
+    user_id bigint REFERENCES users (user_id) ON DELETE CASCADE,
+    query jsonb default '{}'
+);
