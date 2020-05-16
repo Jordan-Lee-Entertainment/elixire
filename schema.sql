e892f6ef318fbb011e42876ef00d3fa51badfec1
Luna <git@l4.pm>
2020-05-15 21:40:54 -0300

    add datadump_queue table

--- schema.sql
+++ schema.sql
@@ -209,3 +209,17 @@ CREATE TABLE IF NOT EXISTS violet_jobs (
     taken_at timestamp without time zone default null,
     internal_state jsonb default '{}'
 );
+
+CREATE TABLE IF NOT EXISTS datadump_queue (
+    job_id uuid primary key,
+    name text unique,
+
+    state bigint default 0,
+    errors text default '',
+    inserted_at timestamp without time zone default (now() at time zone 'utc'),
+    scheduled_at timestamp without time zone default (now() at time zone 'utc'),
+    taken_at timestamp without time zone default null,
+    internal_state jsonb default '{}',
+
+    user_id bigint REFERENCES users (user_id) ON DELETE CASCADE
+);
