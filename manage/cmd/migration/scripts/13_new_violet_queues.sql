2cf425a88956fd272310cbe6d4f0a9e5962802e1
Luna <git@l4.pm>
2020-05-15 21:48:54 -0300

    add 13_new_violet_queues migration

---
+++ manage/cmd/migration/scripts/13_new_violet_queues.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS datadump_queue (
+    job_id uuid primary key,
+    name text unique,
+
+    state bigint default 0,
+    errors text default '',
+    inserted_at timestamp without time zone default (now() at time zone 'utc'),
+    scheduled_at timestamp without time zone default (now() at time zone 'utc'),
+    taken_at timestamp without time zone default null,
+    internal_state jsonb default '{}',
+
+    user_id bigint REFERENCES users (user_id) ON DELETE CASCADE
+);
