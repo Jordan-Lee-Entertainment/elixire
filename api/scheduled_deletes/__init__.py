1521c4cff0736d444d2a9451f88e5d0dbe3dfa62
Luna <git@l4.pm>
2020-05-25 01:06:23 -0300

    add api/scheduled_deletes/__init__.py

---
+++ api/scheduled_deletes/__init__.py
@@ -0,0 +1,7 @@
+# elixire: Image Host software
+# Copyright 2018-2020, elixi.re Team and the elixire contributors
+# SPDX-License-Identifier: AGPL-3.0-only
+
+from .queue import ScheduledDeleteQueue
+
+__all__ = ["ScheduledDeleteQueue"]
