f760ee324f507a67e44b2eb690e79a941daa0224
Luna <git@l4.pm>
2020-05-27 22:30:17 -0300

    handle tuple of file/shorten

--- api/scheduled_deletes/queue.py
+++ api/scheduled_deletes/queue.py
@@ -32,27 +32,28 @@ class ScheduledDeleteQueue(JobQueue):
         file_id: Optional[int] = None,
         **kwargs
     ) -> Flake:
-        return await cls._sched.raw_push(cls, (resource_type, resource_id,), **kwargs)
+        """Push a file or a shorten to be deleted in the future.
+
+        It is recommended:
+         - One of file_id and shorten_id MUST BE passed
+         - scheduled_at is passed
+        """
+        assert shorten_id or file_id
+        return await cls._sched.raw_push(cls, (file_id, shorten_id), **kwargs)
 
     @classmethod
     async def handle(cls, ctx: QueueJobContext):
-        resource_type: str = ctx.args[0]
-        resource_id: int = ctx.args[1]
+        file_id: int = ctx.args[0]
+        shorten_id: int = ctx.args[1]
 
-        assert resource_type in ("file", "shorten")
-        log.info("Got %r %d to be deleted", resource_type, resource_id)
+        assert file_id or shorten_id
 
-        # await app.sched.set_job_state(ctx.job_id, {})
-
-        if resource_type == "file":
-            resource = await File.fetch(resource_id)
-        elif resource_type == "shorten":
-            resource = await Shorten.fetch(resource_id)
+        if file_id is not None:
+            resource = await File.fetch(file_id)
+        elif shorten_id is not None:
+            resource = await Shorten.fetch(shorten_id)
 
         if resource is None:
             return
 
-        ctx.set_start()
-
-        log.info("Resource %r %d successfully deleted", resource_type, resource_id)
         await resource.delete()
