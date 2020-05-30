555bf4a4d8500830b9ffaad73e48f8c001a51e4b
Luna <git@l4.pm>
2020-05-29 23:20:58 -0300

    add impl for scheduled deletion job deletions

--- api/bp/scheduled_deletes.py
+++ api/bp/scheduled_deletes.py
@@ -146,3 +146,51 @@ async def modify_shorten_deletion(shorten_id: int):
         user_id,
         shorten_id=shorten_id,
     )
+
+
+async def revoke_resource_deletion(fetcher_coroutine, user_id, **kwargs):
+    """delete the scheduled deletion timestamp of a given resource
+    by the value given on the request's ``retention_time`` query parameter.
+
+    To guarantee consistency that users wouldn't be able to delete
+    deletions of any resource, ``fetcher_coroutine`` MUST error accordingly.
+    """
+    _ = await fetcher_coroutine
+
+    # try to delete a job that matches the wanted resource
+    file_id: Optional[int] = kwargs.get("file_id")
+    shorten_id: Optional[int] = kwargs.get("shorten_id")
+    assert file_id or shorten_id
+    resource_id: int = file_id or shorten_id
+
+    job_id = await app.db.fetchval(
+        """
+        DELETE FROM scheduled_delete_queue
+        WHERE (file_id = $1 OR shorten_id = $1) AND state = 0
+        RETURNING job_id
+        """,
+        resource_id,
+    )
+
+    if job_id is None:
+        raise NotFound("There were no jobs found for the given resource.")
+
+    return "", 204
+
+
+@bp.route("/files/<file_id>/scheduled_deletion", methods=["DELETE"])
+async def revoke_file_deletion(file_id: int):
+    user_id = await token_check()
+    return await revoke_resource_deletion(
+        File.fetch_by_with_uploader(user_id, file_id=file_id), user_id, file_id=file_id
+    )
+
+
+@bp.route("/shortens/<shorten_id>/scheduled_deletion", methods=["DELETE"])
+async def revoke_shorten_deletion(shorten_id: int):
+    user_id = await token_check()
+    return await revoke_resource_deletion(
+        Shorten.fetch_by_with_uploader(user_id, shorten_id=shorten_id),
+        user_id,
+        shorten_id=shorten_id,
+    )
