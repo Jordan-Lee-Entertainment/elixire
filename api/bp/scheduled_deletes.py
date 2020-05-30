c274923beb5b0f78ce793e5cbd2113ea418b8b57
Luna <git@l4.pm>
2020-05-29 21:51:11 -0300

    generalize scheduled resource deletions

--- api/bp/scheduled_deletes.py
+++ api/bp/scheduled_deletes.py
@@ -12,22 +12,20 @@ from api.scheduled_deletes import (
     validate_request_duration,
     maybe_schedule_deletion,
 )
-from api.models import File
+from api.models import File, Shorten
 
 
 bp = Blueprint("scheduled_deletes", __name__)
 log = logging.getLogger(__name__)
 
 
-@bp.route("/files/<file_id>/scheduled_deletion", methods=["PUT"])
-async def schedule_file_deletion(file_id: int):
-    user_id = await token_check()
+async def schedule_resource_deletion(fetcher_coroutine, user_id: int, **kwargs):
     validate_request_duration(required=True)
 
     # make sure the file exists and uploader matches before
     # scheduling a deletion. method raises NotFound on any mishaps
     # so it will be fine.
-    _ = await File.fetch_by_with_uploader(user_id, file_id=file_id)
+    _ = await fetcher_coroutine
 
     # job_id can't be none because we require retention_time
     # on the validation call. because of that, we also always
@@ -36,7 +34,24 @@ async def schedule_file_deletion(file_id: int):
     #
     # TODO: investigate if we can put a flag on maybe_schedule_deletion
     # so it doesn't request user settings when it isn't required.
-    job_id = await maybe_schedule_deletion(user_id, file_id=file_id)
+    job_id = await maybe_schedule_deletion(user_id, **kwargs)
     assert job_id is not None
-
     return jsonify({"job_id": job_id})
+
+
+@bp.route("/files/<file_id>/scheduled_deletion", methods=["PUT"])
+async def schedule_file_deletion(file_id: int):
+    user_id = await token_check()
+    return await schedule_resource_deletion(
+        File.fetch_by_with_uploader(user_id, file_id=file_id), user_id, file_id=file_id
+    )
+
+
+@bp.route("/shortens/<shorten_id>/scheduled_deletion", methods=["PUT"])
+async def schedule_shorten_deletion(shorten_id: int):
+    user_id = await token_check()
+    return await schedule_resource_deletion(
+        Shorten.fetch_by_with_uploader(shorten_id=shorten_id),
+        user_id,
+        shorten_id=shorten_id,
+    )
