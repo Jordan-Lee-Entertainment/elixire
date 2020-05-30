4e29aee02b134609069ce2eb448f58101efcf4fc
Luna <git@l4.pm>
2020-05-29 22:42:01 -0300

    add impl to modify scheduled deletions

--- api/bp/scheduled_deletes.py
+++ api/bp/scheduled_deletes.py
@@ -3,16 +3,19 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
 import logging
+from typing import Optional
 
-from quart import Blueprint, current_app as app, jsonify
+from quart import Blueprint, current_app as app, jsonify, request
 
 from api.common.auth import token_check
 from api.scheduled_deletes import (
     ScheduledDeleteQueue,
     validate_request_duration,
     maybe_schedule_deletion,
+    extract_scheduled_timestamp,
 )
 from api.models import File, Shorten
+from api.errors import NotFound
 
 
 bp = Blueprint("scheduled_deletes", __name__)
@@ -51,6 +54,67 @@ async def schedule_file_deletion(file_id: int):
 async def schedule_shorten_deletion(shorten_id: int):
     user_id = await token_check()
     return await schedule_resource_deletion(
+        Shorten.fetch_by_with_uploader(user_id, shorten_id=shorten_id),
+        user_id,
+        shorten_id=shorten_id,
+    )
+
+
+async def modify_resource_deletion(fetcher_coroutine, user_id, **kwargs):
+    """Modify the scheduled deletion timestamp of a given resource
+    by the value given on the request's ``retention_time`` query parameter.
+
+    To guarantee consistency that users wouldn't be able to modify
+    deletions of any resource, ``fetcher_coroutine`` MUST error accordingly.
+    """
+    validate_request_duration(required=True)
+    _ = await fetcher_coroutine
+
+    # try to find a job that matches the wanted resource
+    file_id: Optional[int] = kwargs.get("file_id")
+    shorten_id: Optional[int] = kwargs.get("shorten_id")
+    assert file_id or shorten_id
+    resource_id: int = file_id or shorten_id
+
+    column = None
+    if file_id is not None:
+        column = "file_id"
+    elif shorten_id is not None:
+        column = "shorten_id"
+
+    assert column is not None
+
+    _, new_scheduled_at = extract_scheduled_timestamp(request.args["retention_time"])
+    job_id = await app.db.fetchval(
+        f"""
+        UPDATE scheduled_delete_queue
+        SET scheduled_at = $2
+        WHERE {column} = $1 AND state = 0
+        RETURNING job_id
+        """,
+        resource_id,
+        new_scheduled_at,
+    )
+
+    if job_id is None:
+        # TODO: maybe this error is too generic?
+        raise NotFound("There were no jobs found for the given resource.")
+
+    return "", 204
+
+
+@bp.route("/files/<file_id>/scheduled_deletion", methods=["PATCH"])
+async def modify_file_deletion(file_id: int):
+    user_id = await token_check()
+    return await modify_resource_deletion(
+        File.fetch_by_with_uploader(user_id, file_id=file_id), user_id, file_id=file_id
+    )
+
+
+@bp.route("/shortens/<shorten_id>/scheduled_deletion", methods=["PATCH"])
+async def modify_shorten_deletion(shorten_id: int):
+    user_id = await token_check()
+    return await modify_resource_deletion(
         Shorten.fetch_by_with_uploader(user_id, shorten_id=shorten_id),
         user_id,
         shorten_id=shorten_id,
