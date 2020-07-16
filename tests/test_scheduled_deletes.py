cfd5b5879ed4c860712473ef57bf1d8d77d13968
Luna <git@l4.pm>
2020-07-15 22:51:30 -0300

    add test to patch scheduled deletes

--- tests/test_scheduled_deletes.py
+++ tests/test_scheduled_deletes.py
@@ -2,6 +2,7 @@
 # Copyright 2018-2020, elixi.re Team and the elixire contributors
 # SPDX-License-Identifier: AGPL-3.0-only
 
+import time
 import pytest
 from api.models import File, Shorten
 from api.scheduled_deletes import ScheduledDeleteQueue
@@ -56,3 +57,43 @@ async def test_existing_shorten(test_cli_user):
         postwait_shorten = await Shorten.fetch(shorten.id)
         assert postwait_shorten is not None
         assert postwait_shorten.deleted
+
+
+async def test_scheduled_patch(test_cli_user):
+    elixire_file = await test_cli_user.create_file("test.png", png_data(), "image/png")
+
+    resp = await test_cli_user.patch(
+        f"/api/files/{elixire_file.id}/scheduled_deletion",
+        query_string={"retention_time": "PT3S"},
+    )
+    assert resp.status_code == 404
+
+    # insert scheduled deletion to 10 mins
+    resp = await test_cli_user.put(
+        f"/api/files/{elixire_file.id}/scheduled_deletion",
+        query_string={"retention_time": "PT40S"},
+    )
+    assert resp.status_code == 200
+    rjson = await resp.json
+    assert isinstance(rjson, dict)
+    job_id = rjson["job_id"]
+
+    resp = await test_cli_user.patch(
+        f"/api/files/{elixire_file.id}/scheduled_deletion",
+        query_string={"retention_time": "PT3S"},
+    )
+    assert resp.status_code == 204
+
+    # sure, if the patch doesn't work, it's likely we take some
+    # time in this test. that's why it'd fail.
+    t1 = time.monotonic()
+    await ScheduledDeleteQueue.wait_job(job_id)
+    t2 = time.monotonic()
+    seconds = t2 - t1
+
+    assert seconds < 10
+
+    async with test_cli_user.app.app_context():
+        postwait_file = await File.fetch(elixire_file.id)
+        assert postwait_file is not None
+        assert postwait_file.deleted
