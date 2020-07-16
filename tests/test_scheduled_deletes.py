d36b9c5b4bd9e19ea35e33e0e9a5a66808c4b4b1
Luna <git@l4.pm>
2020-07-16 00:31:53 -0300

    add test for deletion of scheduled entries

--- tests/test_scheduled_deletes.py
+++ tests/test_scheduled_deletes.py
@@ -3,7 +3,9 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
 import time
+
 import pytest
+
 from api.models import File, Shorten
 from api.scheduled_deletes import ScheduledDeleteQueue
 from .common import png_data
@@ -68,7 +70,7 @@ async def test_scheduled_patch(test_cli_user):
     )
     assert resp.status_code == 404
 
-    # insert scheduled deletion to 10 mins
+    # insert scheduled deletion, and ensure we can modify it
     resp = await test_cli_user.put(
         f"/api/files/{elixire_file.id}/scheduled_deletion",
         query_string={"retention_time": "PT40S"},
@@ -97,3 +99,29 @@ async def test_scheduled_patch(test_cli_user):
         postwait_file = await File.fetch(elixire_file.id)
         assert postwait_file is not None
         assert postwait_file.deleted
+
+
+async def test_scheduled_delete(test_cli_user):
+    elixire_file = await test_cli_user.create_file("test.png", png_data(), "image/png")
+
+    resp = await test_cli_user.put(
+        f"/api/files/{elixire_file.id}/scheduled_deletion",
+        query_string={"retention_time": "PT2S"},
+    )
+    assert resp.status_code == 200
+    rjson = await resp.json
+    assert isinstance(rjson, dict)
+    job_id = rjson["job_id"]
+
+    resp = await test_cli_user.delete(
+        f"/api/files/{elixire_file.id}/scheduled_deletion",
+    )
+    assert resp.status_code == 204
+
+    try:
+        await ScheduledDeleteQueue.wait_job(job_id)
+        assert False
+    except ValueError:
+        # If this timeouts, then the job id was likely deleted and there isn't
+        # an entry on violet about this waiting.
+        pass
