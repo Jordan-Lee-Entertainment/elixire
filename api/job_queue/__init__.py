48d4f9f217bfe61c226c14c6c1082f21b0b54773
Luna <git@l4.pm>
2020-07-16 15:47:55 -0300

    rm api/job_queue

--- api/job_queue/__init__.py
+++
@@ -1,6 +0,0 @@
-# elixire: Image Host software
-# Copyright 2018-2020, elixi.re Team and the elixire contributors
-# SPDX-License-Identifier: AGPL-3.0-only
-from .auto_deletes import AutoDeleteQueue
-
-__all__ = ["AutoDeleteQueue"]
