fa58c7aa84a6e2d87d9ddc69027a03310a390a01
Ave <ave@ave.zone>
2020-09-16 20:35:12 +0300

    fetch: Disable caching on selective serving for Discordbot

--- api/bp/fetch.py
+++ api/bp/fetch.py
@@ -60,8 +60,11 @@ async def file_handler(request, filename):
         return response.html("""
 <html>
     <head>
-        <meta property="twitter:card" content="summary_large_image">
-        <meta property="twitter:image" content="{}">
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:image" content="{}" />
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+        <meta http-equiv="Pragma" content="no-cache" />
+        <meta http-equiv="Expires" content="0" />
     </head>
 </html>""".format(raw_url))
 
