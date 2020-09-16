0c0271dff0ef2465fa90e2add0fab97a389ab905
Ave <ave@ave.zone>
2020-09-16 23:00:10 +0300

    Revert "fetch: Disable caching on selective serving for Discordbot"
    This reverts commit fa58c7aa84a6e2d87d9ddc69027a03310a390a01.

--- api/bp/fetch.py
+++ api/bp/fetch.py
@@ -60,11 +60,8 @@ async def file_handler(request, filename):
         return response.html("""
 <html>
     <head>
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:image" content="{}" />
-        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-        <meta http-equiv="Pragma" content="no-cache" />
-        <meta http-equiv="Expires" content="0" />
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="twitter:image" content="{}">
     </head>
 </html>""".format(raw_url))
 
