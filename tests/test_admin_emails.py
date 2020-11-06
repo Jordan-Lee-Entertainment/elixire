ddb860160be75c6caf8cf4de1aa4f793f286794a
Luna <git@l4.pm>
2020-11-06 00:17:20 -0300

    add test for admin sendmail

--- tests/test_admin_emails.py
+++ tests/test_admin_emails.py
@@ -20,4 +20,24 @@ async def test_broadcast(test_cli_admin):
     # TODO https://gitlab.com/elixire/elixire/-/issues/172
     # email = test_cli_admin.app._email_list[-1]
     # assert email["subject"] == subject
-    # assert email["body"] == body
+    # assert email["content"] == body
+
+
+async def test_sendmail(test_cli_admin):
+    subject = username()
+    body = username()
+
+    user = await test_cli_admin.create_user()
+
+    resp = await test_cli_admin.post(
+        f"/api/admin/email/{user.id}", json={"subject": subject, "body": body}
+    )
+    assert resp.status_code == 200
+    rjson = await resp.json
+    assert isinstance(rjson, dict)
+    assert rjson["email"] == user.email
+
+    email = test_cli_admin.app._email_list[-1]
+    assert email["email"] == user.email
+    assert email["subject"] == subject
+    assert email["content"] == body
