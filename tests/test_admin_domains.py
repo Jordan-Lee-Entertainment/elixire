756d7e9a2b893022c4e70ccab22447abba330047
Luna <git@l4.pm>
2020-11-06 16:43:00 -0300

    add test_admin_domains

---
+++ tests/test_admin_domains.py
@@ -0,0 +1,33 @@
+# elixire: Image Host software
+# Copyright 2018-2020, elixi.re Team and the elixire contributors
+# SPDX-License-Identifier: AGPL-3.0-only
+import pytest
+
+from tests.common.generators import username
+from api.models import Domain
+
+pytestmark = pytest.mark.asyncio
+
+
+async def test_admin_create_domain(test_cli_admin):
+    domain_name = f"{username()}.com"
+
+    resp = await test_cli_admin.put(
+        "/api/admin/domains",
+        json={
+            "domain": domain_name,
+            "permissions": 3,
+            "owner_id": test_cli_admin["user_id"],
+        },
+    )
+
+    assert resp.status_code == 200
+    rjson = await resp.json
+    assert isinstance(rjson, dict)
+    domain = rjson["domain"]
+    assert isinstance(domain, dict)
+    assert domain["domain"] == domain_name
+
+    async with test_cli_admin.app.app_context():
+        domain = await Domain.fetch(domain["id"])
+        test_cli_admin.add_resource(domain)
