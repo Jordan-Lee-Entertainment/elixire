c7b55e47a1081395267e89cc5d8cfd646f011575
Luna <git@l4.pm>
2020-12-11 15:07:08 -0300

    banning: ban respective /64 on any ipv6 address

--- api/common/banning.py
+++ api/common/banning.py
@@ -70,6 +70,9 @@ async def ban_request(reason: str) -> None:
         await ban_user(user_id, reason)
     except AttributeError:
         inet = ipaddress.ip_network(get_ip_addr())
+        if isinstance(inet, ipnetwork.IPv6Network):
+            inet = inet.supernet(new_prefix=64)
+
         log.warning(f"Banning ip address {inet} with reason {reason!r}")
         await ban_by_ip(inet, reason)
 
