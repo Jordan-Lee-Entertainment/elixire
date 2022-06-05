96e9c6afe32610f2bd68aa21307a72affea15d80
Luna <git@l4.pm>
2022-06-05 02:21:45 -0300

    test_upload: migrate to test_cli_user

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -12,19 +12,14 @@ from urllib.parse import urlparse
 from quart.testing import make_test_body_with_headers
 from quart.datastructures import FileStorage
 
-from .common import login_normal, login_admin, png_data, hexs
+from .common import login_admin, png_data, hexs
 
 pytestmark = pytest.mark.asyncio
 
 
-async def check_exists(test_cli, shortname, utoken, not_exists=False):
+async def check_exists(test_cli, shortname, not_exists=False):
     """Check if a file exists, given the shortname, token, etc."""
-    resp = await test_cli.get(
-        "/api/list?page=0",
-        headers={
-            "Authorization": utoken,
-        },
-    )
+    resp = await test_cli.get("/api/list?page=0")
 
     assert resp.status_code == 200
     rjson = await resp.json
@@ -44,22 +39,27 @@ async def check_exists(test_cli, shortname, utoken, not_exists=False):
 
     relative_image_path = f"/i/{shortname}{extension}"
     # check the file is available on the domain+subdomain it was uploaded on
-    resp = await test_cli.get(relative_image_path, headers={"host": url.netloc})
+    resp = await test_cli.get(
+        relative_image_path, do_token=False, headers={"host": url.netloc}
+    )
     assert resp.status_code == 200
 
     # check the file isn't available on other domains
-    resp = await test_cli.get(relative_image_path, headers={"host": "undefined.com"})
+    resp = await test_cli.get(
+        relative_image_path, do_token=False, headers={"host": "undefined.com"}
+    )
     assert resp.status_code == 404
 
     # check thumbnail can be generated successfully
     resp = await test_cli.get(
-        f"/t/s{shortname}{extension}", headers={"host": url.netloc}
+        f"/t/s{shortname}{extension}", do_token=False, headers={"host": url.netloc}
     )
     assert resp.status_code == 200
 
     # check that Discordbot receives html
     resp = await test_cli.get(
         relative_image_path,
+        do_token=False,
         headers={"host": url.netloc, "user-agent": "Discordbot"},
     )
     assert resp.status_code == 200
@@ -80,11 +80,15 @@ async def check_exists(test_cli, shortname, utoken, not_exists=False):
     # assert "twitter" in response_text
 
     # -- test HEAD and Ranged requests to the file
-    resp = await test_cli.head(relative_image_path, headers={"host": url.netloc})
+    resp = await test_cli.head(
+        relative_image_path, do_token=False, headers={"host": url.netloc}
+    )
     assert resp.status_code == 200
 
     resp = await test_cli.get(
-        relative_image_path, headers={"host": url.netloc, "range": "bytes=0-9"}
+        relative_image_path,
+        do_token=False,
+        headers={"host": url.netloc, "range": "bytes=0-9"},
     )
     assert resp.status_code == 206
     assert resp.headers["content-length"] == "10"
@@ -106,12 +110,10 @@ def png_request(data=None):
     return {"data": body, "headers": headers}
 
 
-async def test_upload_png(test_cli):
+async def test_upload_png(test_cli_user):
     """Test that the upload route works given test data"""
-    utoken = await login_normal(test_cli)
     kwargs = png_request()
-    kwargs["headers"]["authorization"] = utoken
-    resp = await test_cli.post(
+    resp = await test_cli_user.post(
         "/api/upload",
         **kwargs,
     )
@@ -123,12 +125,13 @@ async def test_upload_png(test_cli):
     assert isinstance(respjson["delete_url"], str)
 
     shortname = respjson["shortname"]
-    await check_exists(test_cli, shortname, utoken)
+    await check_exists(test_cli_user, shortname)
 
     # -- test fetching file on admin api
-    atoken = await login_admin(test_cli)
-    resp = await test_cli.get(
+    atoken = await login_admin(test_cli_user)
+    resp = await test_cli_user.get(
         f"/api/admin/file/{shortname}",
+        do_token=False,
         headers={"authorization": atoken},
     )
     assert resp.status_code == 200
@@ -136,22 +139,19 @@ async def test_upload_png(test_cli):
     assert rjson["filename"] == shortname
 
 
-async def test_delete_file(test_cli):
-    utoken = await login_normal(test_cli)
+async def test_delete_file(test_cli_user):
     kwargs = png_request()
-    kwargs["headers"]["authorization"] = utoken
-    resp = await test_cli.post("/api/upload", **kwargs)
+    resp = await test_cli_user.post("/api/upload", **kwargs)
 
     assert resp.status_code == 200
     respjson = await resp.json
     assert isinstance(respjson, dict)
     assert isinstance(respjson["url"], str)
-    await check_exists(test_cli, respjson["shortname"], utoken)
+    await check_exists(test_cli_user, respjson["shortname"])
 
     # test delete
-    resp_del = await test_cli.delete(
+    resp_del = await test_cli_user.delete(
         "/api/delete",
-        headers={"Authorization": utoken},
         json={"filename": respjson["shortname"]},
     )
 
@@ -160,14 +160,12 @@ async def test_delete_file(test_cli):
     assert isinstance(rdel_json, dict)
     assert rdel_json["success"]
 
-    await check_exists(test_cli, respjson["shortname"], utoken, True)
+    await check_exists(test_cli_user, respjson["shortname"], True)
 
 
-async def test_delete_nonexist(test_cli):
-    utoken = await login_normal(test_cli)
-    resp_del = await test_cli.delete(
+async def test_delete_nonexist(test_cli_user):
+    resp_del = await test_cli_user.delete(
         "/api/delete",
-        headers={"Authorization": utoken},
         json={"filename": "lkdjklfjkgkghkkhsfklhjslkdfjglakdfjl"},
     )
 
@@ -177,35 +175,30 @@ async def test_delete_nonexist(test_cli):
 EICAR = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"
 
 
-async def test_eicar_upload(test_cli):
-    if not test_cli.app.econfig.UPLOAD_SCAN:
+async def test_eicar_upload(test_cli_user):
+    if not test_cli_user.app.econfig.UPLOAD_SCAN:
         pytest.skip("upload scans disabled")
         return
-
-    utoken = await login_normal(test_cli)
 
     # without this, asyncio subprocess just hangs on communicate(), even
     # though the process already finished. its so fuckin weird
     #
     # fix found on https://github.com/python/asyncio/issues/478#issuecomment-268476438
-    asyncio.get_child_watcher().attach_loop(test_cli.app.loop)
+    asyncio.get_child_watcher().attach_loop(test_cli_user.app.loop)
 
-    test_cli.app.econfig.SCAN_WAIT_THRESHOLD = 5
+    test_cli_user.app.econfig.SCAN_WAIT_THRESHOLD = 5
 
     kwargs = png_request(data=io.BytesIO(EICAR.encode()))
-    kwargs["headers"]["authorization"] = utoken
-    resp = await test_cli.post(
+    resp = await test_cli_user.post(
         "/api/upload",
         **kwargs,
     )
     assert resp.status_code == 415
 
 
-async def test_upload_random_domain(test_cli):
-    utoken = await login_normal(test_cli)
+async def test_upload_random_domain(test_cli_user):
     kwargs = png_request()
-    kwargs["headers"]["authorization"] = utoken
-    resp = await test_cli.post(
+    resp = await test_cli_user.post(
         "/api/upload",
         **kwargs,
     )
@@ -217,4 +210,4 @@ async def test_upload_random_domain(test_cli):
     assert isinstance(respjson["delete_url"], str)
 
     shortname = respjson["shortname"]
-    await check_exists(test_cli, shortname, utoken)
+    await check_exists(test_cli_user, shortname)
